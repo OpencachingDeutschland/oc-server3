84552e9e4ecb5033a6deb4a037b63a27b690894f
following <following>
2012-08-05 20:50:34 +0200

    Gleichsetzung Produktivsystem: überflüssige Tabellen

--- htdocs/doc/sql/tables/ws_tan.sql
+++
@@ -1,8 +0,0 @@
-SET NAMES 'utf8';
-DROP TABLE IF EXISTS `ws_tan`;
-CREATE TABLE `ws_tan` (
-  `session` varchar(36) NOT NULL,
-  `tan` varchar(36) NOT NULL,
-  PRIMARY KEY  (`session`,`tan`),
-  UNIQUE KEY `tan` (`tan`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;

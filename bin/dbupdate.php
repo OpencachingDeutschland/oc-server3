038719fa3cf8a7d791e6f937a70e9fa2b097c0e5
following <following>
2012-08-24 18:15:53 +0200

    made dbupdate.php independend from cwd

--- bin/dbupdate.php
+++ bin/dbupdate.php
@@ -12,18 +12,18 @@
 	 * Tables must be updated manually, see htdocs/doc/sql/db-changes.txt
 	 */ 
   
-	require('htdocs/lib/settings.inc.php');
+  $rootpath = dirname(__FILE__) . '/..';
+  require($rootpath . '/htdocs/lib/settings.inc.php');
 	
   echo "importing data.sql ...\n";
-  chdir ('htdocs/doc/sql');
-	system('cat static-data/data.sql | mysql -u' . $dbusername . ' --password=' . $dbpasswd . ' ' . $dbname);
+  system('cat ' . $rootpath . '/htdocs/doc/sql/static-data/data.sql | mysql -u' . $dbusername . ' --password=' . $dbpasswd . ' ' . $dbname);
 
   echo "importing triggers ...\n";
-  chdir('stored-proc');
+  chdir ($rootpath . '/htdocs/doc/sql/stored-proc');
   system('php maintain.php');
 
   echo "resettings webcache ...\n";
-  chdir('../../../../bin');
+  chdir ($rootpath . "/bin");
   system('php clear-webcache.php');
   
 ?>

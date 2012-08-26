OpencachingDeutschland/oc-server3

commit 016f3c7da0d4b8df8598b80cfa01decec700f281

638: fixed log edit permissions for locked caches

htdocs/templates2/ocstyle/res_logentry_logitem.tpl
@@ -33,17 +33,17 @@
 
 	{if $cache.userid==$login.userid || $logItem.userid==$login.userid}
 		<p style="font-weight: 400;display:inline;"><img src="images/trans.gif" border="0" width="16" height="16" alt="" title="" />
-			{if $logItem.userid==$login.userid}
+			{if $logItem.userid==$login.userid && ($cache.userid==$login.userid || $cache.status!=6)}
 				<a href="editlog.php?logid={$logItem.id|urlencode}"><img src="resource2/{$opt.template.style}/images/action/16x16-properties.png" border="0" align="middle" border="0" width="16" height="16" alt="" /></a>
 				[<a href="editlog.php?logid={$logItem.id|urlencode}">{t}Edit{/t}</a>]
 			{/if}
 
-			{if $cache.userid==$login.userid || $logItem.userid==$login.userid}
+			{if $logItem.userid==$login.userid}
 				<a href="removelog.php?logid={$logItem.id|urlencode}"><img src="resource2/{$opt.template.style}/images/log/16x16-trash.png" border="0" align="middle" border="0" width="16" height="16" alt="" /></a>
 				[<a href="removelog.php?logid={$logItem.id|urlencode}">{t}Delete{/t}</a>]
 			{/if}
 
-			{if $logItem.userid==$login.userid}
+			{if $logItem.userid==$login.userid && $cache.status!=6}
 				<a href="picture.php?action=add&loguuid={$logItem.uuid|urlencode}"><img src="resource2/{$opt.template.style}/images/action/16x16-addimage.png" border="0" align="middle" border="0" width="16" height="16" alt="" /></a>
 				[<a href="picture.php?action=add&loguuid={$logItem.uuid|urlencode}">{t}Upload picture{/t}</a>]
 			{/if}

htdocs/editlog.php
@@ -50,11 +50,27 @@
 		else
 		{
 			//does log with this logid exist?
-			$log_rs = sql("SELECT `cache_logs`.`cache_id` AS `cache_id`, `cache_logs`.`node` AS `node`, `cache_logs`.`text` AS `text`, `cache_logs`.`date` AS `date`, `cache_logs`.`user_id` AS `user_id`, `cache_logs`.`type` AS `logtype`, `cache_logs`.`text_html` AS `text_html`, `cache_logs`.`text_htmledit` AS `text_htmledit`, `caches`.`name` AS `cachename`, `caches`.`type` AS `cachetype`, `caches`.`user_id` AS `cache_user_id`, `caches`.`logpw` as `logpw`, `caches`.`status` as `status` FROM `cache_logs` INNER JOIN `caches` ON (`caches`.`cache_id`=`cache_logs`.`cache_id`) WHERE `id`='&1'", $log_id);
+			$log_rs = sql("SELECT `cache_logs`.`cache_id` AS `cache_id`, 
+														`cache_logs`.`node` AS `node`, 
+														`cache_logs`.`text` AS `text`, 
+														`cache_logs`.`date` AS `date`, 
+														`cache_logs`.`user_id` AS `user_id`, 
+														`cache_logs`.`type` AS `logtype`, 
+														`cache_logs`.`text_html` AS `text_html`, 
+														`cache_logs`.`text_htmledit` AS `text_htmledit`, 
+														`caches`.`name` AS `cachename`, 
+														`caches`.`type` AS `cachetype`, 
+														`caches`.`user_id` AS `cache_user_id`, 
+														`caches`.`logpw` as `logpw`, 
+														`caches`.`status` as `status` 
+											FROM `cache_logs` 
+											INNER JOIN `caches` ON (`caches`.`cache_id`=`cache_logs`.`cache_id`) WHERE `id`='&1'", 
+											$log_id);
 			$log_record = sql_fetch_array($log_rs);
 			sql_free_result($log_rs);
-
-			if ($log_record !== false && $log_record['status'] != 6 && $log_record['status'] != 7)
+			if ($log_record !== false && 
+					($log_record['status'] != 6 || ($log_record['cache_user_id'] == $login->userid && $log_record['user_id'] == $login->userid)) &&   
+					$log_record['status'] != 7)
 			{
 				require($stylepath . '/editlog.inc.php');
 				require($stylepath.'/rating.inc.php');

commit 53b73481f5e07f570fe21e7f1671af2bfabddd3f

log permission fixes
- fixed bug introduced with commit 016f3c7d
- fixed log edit links for locked caches when using viewlogs.php
    (added `status` to SELECT statement)

htdocs/templates2/ocstyle/res_logentry_logitem.tpl
@@ -38,7 +38,7 @@
 				[<a href="editlog.php?logid={$logItem.id|urlencode}">{t}Edit{/t}</a>]
 			{/if}
 
-			{if $logItem.userid==$login.userid}
+			{if $cache.userid==$login.userid || $logItem.userid==$login.userid}
 				<a href="removelog.php?logid={$logItem.id|urlencode}"><img src="resource2/{$opt.template.style}/images/log/16x16-trash.png" border="0" align="middle" border="0" width="16" height="16" alt="" /></a>
 				[<a href="removelog.php?logid={$logItem.id|urlencode}">{t}Delete{/t}</a>]
 			{/if}

htdocs/viewlogs.php
@@ -39,7 +39,17 @@
 	if ($cache_id != 0)
 	{ 
 		//get cache record
-		$rs = sql("SELECT `caches`.`cache_id`, `caches`.`wp_oc` AS `wpoc`, `caches`.`user_id` AS `userid`, `caches`.`name`, IFNULL(`stat_caches`.`found`, 0) AS `found`, IFNULL(`stat_caches`.`notfound`, 0) AS `notfound`, IFNULL(`stat_caches`.`note`, 0) AS `note`, `cache_status`.`allow_user_view` FROM `caches` INNER JOIN `cache_status` ON `caches`.`status`=`cache_status`.`id` LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id` WHERE `caches`.`cache_id`='&1'", $cache_id);
+		$rs = sql("SELECT `caches`.`cache_id`, `caches`.`wp_oc` AS `wpoc`, 
+											`caches`.`user_id` AS `userid`, `caches`.`name`, 
+											`caches`.`status` AS `status`,
+											IFNULL(`stat_caches`.`found`, 0) AS `found`, 
+											IFNULL(`stat_caches`.`notfound`, 0) AS `notfound`, 
+											IFNULL(`stat_caches`.`note`, 0) AS `note`, 
+											`cache_status`.`allow_user_view` 
+							 FROM `caches` 
+							 INNER JOIN `cache_status` ON `caches`.`status`=`cache_status`.`id` 
+							 LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id` 
+							 WHERE `caches`.`cache_id`='&1'", $cache_id);
 		$rCache = sql_fetch_array($rs);
 		sql_free_result($rs);
 
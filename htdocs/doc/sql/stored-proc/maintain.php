OpencachingDeutschland/oc-server3

commit c32a4b06ffac2e9f788a1c1c5b9e219ce120dbc4

improved start page performance; top rating statistics was very slow
needs a new database field with manual init, see db-changes.txt !

and fixed another recommendation bug:
- recommendations will not get lost when a user edits one of multiple logs
  for the same cache

htdocs/doc/sql/stored-proc/maintain.php
@@ -668,6 +668,13 @@
 						INSERT IGNORE INTO `removed_objects` (`localId`, `uuid`, `type`, `node`) VALUES (OLD.`id`, OLD.`uuid`, 1, OLD.`node`);
 					END;");
 
+	sql_dropTrigger('cacheRatingBeforeInsert');
+	sql("CREATE TRIGGER `cacheRatingBeforeInsert` BEFORE INSERT ON `cache_rating`
+				FOR EACH ROW
+					BEGIN
+						SET NEW.`rating_date` = NOW();
+					END;");
+
 	sql_dropTrigger('cacheRatingAfterInsert');
 	sql("CREATE TRIGGER `cacheRatingAfterInsert` AFTER INSERT ON `cache_rating` 
 				FOR EACH ROW 

htdocs/editlog.php
@@ -79,7 +79,7 @@
 					$log_date_day = isset($_POST['logday']) ? $_POST['logday'] : date('d', strtotime($log_record['date']));
 					$log_date_month = isset($_POST['logmonth']) ? $_POST['logmonth'] : date('m', strtotime($log_record['date']));
 					$log_date_year = isset($_POST['logyear']) ? $_POST['logyear'] : date('Y', strtotime($log_record['date']));
-					$top_cache = isset($_POST['rating']) ? $_POST['rating']+0 : 0;
+					$top_cache = isset($_POST['rating']) ? $_POST['rating']+0 : null;
 
 					$log_pw = '';
 					$use_log_pw = (($log_record['logpw'] == NULL) || ($log_record['logpw'] == '')) ? false : true;
@@ -98,7 +98,7 @@
 					{
 						if (($user_founds * rating_percentage/100) < 1)
 						{
-							$top_cache = 0;
+							$top_cache = null;
 							$anzahl = (1 - ($user_founds * rating_percentage/100)) / (rating_percentage/100);
 							if ($anzahl > 1)
 							{
@@ -117,7 +117,7 @@
 						}
 						else
 						{
-							$top_cache = 0;
+							$top_cache = null;
 							$anzahl = ($user_tops + 1 - ($user_founds * rating_percentage/100)) / (rating_percentage/100);
 							if ($anzahl > 1)
 							{
@@ -243,7 +243,7 @@
 					// not a found log? then ignore the rating
 					if ($log_type != 1 && $log_type != 7)
 					{
-						$top_cache = 0;
+						$top_cache = null;
 					}
 
 
@@ -293,7 +293,7 @@
 						// update top-list
 						if ($top_cache == 1)
 							sql("INSERT IGNORE INTO `cache_rating` (`user_id`, `cache_id`) VALUES('&1', '&2')", $usr['userid'], $log_record['cache_id']);
-						else
+						else if ($top_cache === 0)   // 0 but not null
 							sql("DELETE FROM `cache_rating` WHERE `user_id`='&1' AND `cache_id`='&2'", $usr['userid'], $log_record['cache_id']);
 
 						// do not use slave server for the next time ...

htdocs/index.php
@@ -63,12 +63,6 @@
 														`user`.`username` `username`,
 														`caches`.`cache_id` `cache_id`,
 														`caches`.`name` `name`,
-														`caches`.`longitude` `longitude`,
-														`caches`.`latitude` `latitude`,
-														`caches`.`date_created` `date_created`,
-														`caches`.`country` `country`,
-														`caches`.`difficulty` `difficulty`,
-														`caches`.`terrain` `terrain`,
 														`caches`.`date_hidden`,
 														`cache_location`.`adm1`,
 														`cache_location`.`adm2`,
@@ -91,13 +85,7 @@
 														`user`.`username` `username`,
 														`caches`.`cache_id` `cache_id`,
 														`caches`.`name` `name`,
-														`caches`.`longitude` `longitude`,
-														`caches`.`latitude` `latitude`,
 														`caches`.`date_created` `date_created`,
-														`caches`.`country` `country`,
-														`caches`.`difficulty` `difficulty`,
-														`caches`.`terrain` `terrain`,
-														`caches`.`date_hidden`,
 														`caches`.`type`,
 														`cache_location`.`adm1`,
 														`cache_location`.`adm2`,
@@ -115,39 +103,33 @@
 		sql_free_result($rs);
 
 		// last 30 days' top ratings
-		$rs = sql_slave("SELECT COUNT(DISTINCT `cache_logs`.`user_id`) AS `cRatings`, 
-																	`cache_logs`.`cache_id`, 
-																	MAX(`cache_logs`.`date`) AS `dLastLog`, 
+		//
+		// 2012-08-24 following
+		//		optimized by adding rating_date field to cache_rating, so we don't need the log table. 
+
+		$rs = sql_slave("SELECT COUNT(`cache_rating`.`user_id`) AS `cRatings`, 
+																	MAX(`cache_rating`.`rating_date`) AS `dLastLog`, 
 																	`user`.`user_id` AS `user_id`,
 																	`user`.`username` AS `username`,
 																	`caches`.`cache_id` AS `cache_id`,
 																	`caches`.`name` AS `name`,
-																	`caches`.`longitude` AS `longitude`,
-																	`caches`.`latitude` AS `latitude`,
-																	`caches`.`date_created` AS `date_created`,
-																	`caches`.`country` AS `country`,
-																	`caches`.`difficulty` AS `difficulty`,
-																	`caches`.`terrain` AS `terrain`,
-																	`caches`.`date_hidden`,
 																	`caches`.`type`,
 																	`cache_location`.`adm1`,
 																	`cache_location`.`adm2`,
 																	`cache_location`.`adm3`,
 																	`cache_location`.`adm4`
-														 FROM `cache_logs` 
-											 INNER JOIN `cache_rating` ON `cache_logs`.`cache_id`=`cache_rating`.`cache_id` AND 
-																										`cache_logs`.`user_id`=`cache_rating`.`user_id` 
-											 INNER JOIN `caches` ON `cache_logs`.`cache_id`=`caches`.`cache_id`
-											 INNER JOIN `user` ON `user`.`user_id`=`caches`.`user_id`
-												LEFT JOIN `cache_location` ON `caches`.`cache_id`=`cache_location`.`cache_id`
+														 FROM `cache_rating` 
+											 INNER JOIN `caches` ON `caches`.`cache_id`=`cache_rating`.`cache_id`
+											 INNER JOIN `user` ON `user`.`user_id`=`cache_rating`.`user_id`
+												LEFT JOIN `cache_location` ON `cache_rating`.`cache_id`=`cache_location`.`cache_id`
 														WHERE `caches`.`country`='&1' AND 
-														      `cache_logs`.`type`=1 AND 
-																	`cache_logs`.`date`>DATE_SUB(NOW(), INTERVAL 30 DAY) AND 
+																	`cache_rating`.`rating_date`>DATE_SUB(NOW(), INTERVAL 30 DAY) AND 
 																	`caches`.`type`!=6 AND 
 																	`caches`.`status`=1
-												 GROUP BY `cache_logs`.`cache_id` 
+												 GROUP BY `cache_rating`.`cache_id` 
 												 ORDER BY `cRatings` DESC, 
-																	`dLastLog` DESC 
+																	`dLastLog` DESC,
+																	`cache_id` DESC 
 														LIMIT 0, 10",
 									                $sUserCountry);
 		$tpl->assign_rs('topratings', $rs);

commit 9bc361c3e3c2945a1f75c3267d5e948ddc5f6e19

fixed rating bugs introduced with commit c32a4b06

htdocs/doc/sql/stored-proc/maintain.php
@@ -668,11 +668,14 @@
 						INSERT IGNORE INTO `removed_objects` (`localId`, `uuid`, `type`, `node`) VALUES (OLD.`id`, OLD.`uuid`, 1, OLD.`node`);
 					END;");
 
+	// IF condition is defined to work with both, rating_date field may be NULL or not
 	sql_dropTrigger('cacheRatingBeforeInsert');
 	sql("CREATE TRIGGER `cacheRatingBeforeInsert` BEFORE INSERT ON `cache_rating`
 				FOR EACH ROW
 					BEGIN
-						SET NEW.`rating_date` = NOW();
+						IF ISNULL(NEW.`rating_date`) OR NEW.`rating_date` < '2000' THEN
+							SET NEW.`rating_date` = NOW(); 
+						END IF;
 					END;");
 
 	sql_dropTrigger('cacheRatingAfterInsert');

htdocs/editlog.php
@@ -212,6 +212,8 @@
 					//store?
 					if (isset($_POST['submitform']) && $date_ok && $logtype_ok && $pw_ok)
 					{
+						$log_date = date('Y-m-d', mktime(0, 0, 0, $log_date_month, $log_date_day, $log_date_year));
+
 						//store changed data
 						sql("UPDATE `cache_logs` SET `type`='&1',
 						                             `date`='&2',
@@ -220,7 +222,7 @@
 						                             `text_htmledit`='&5'
 						                       WHERE `id`='&6'",
 						                             $log_type,
-						                             date('Y-m-d', mktime(0, 0, 0, $log_date_month, $log_date_day, $log_date_year)),
+						                             $log_date,
 						                             (($descMode != 1) ? $log_text : nl2br($log_text)),
 						                             (($descMode != 1) ? 1 : 0),
 						                             (($descMode == 3) ? 1 : 0),
@@ -237,7 +239,10 @@
 						// update top-list
 						if ($top_option)
 							if ($top_cache)
-								sql("INSERT IGNORE INTO `cache_rating` (`user_id`, `cache_id`) VALUES('&1', '&2')", $usr['userid'], $log_record['cache_id']);
+								sql("INSERT INTO `cache_rating` (`user_id`, `cache_id`, `rating_date`)
+					 					 VALUES('&1', '&2','&3')
+										 ON DUPLICATE KEY UPDATE `rating_date`='&3'",
+										 $usr['userid'], $log_record['cache_id'], $log_date);
 							else
 								sql("DELETE FROM `cache_rating` WHERE `user_id`='&1' AND `cache_id`='&2'", $usr['userid'], $log_record['cache_id']);
 

htdocs/index.php
@@ -120,7 +120,7 @@
 																	`cache_location`.`adm4`
 														 FROM `cache_rating` 
 											 INNER JOIN `caches` ON `caches`.`cache_id`=`cache_rating`.`cache_id`
-											 INNER JOIN `user` ON `user`.`user_id`=`cache_rating`.`user_id`
+											 INNER JOIN `user` ON `user`.`user_id`=`caches`.`user_id`
 												LEFT JOIN `cache_location` ON `cache_rating`.`cache_id`=`cache_location`.`cache_id`
 														WHERE `caches`.`country`='&1' AND 
 																	`cache_rating`.`rating_date`>DATE_SUB(NOW(), INTERVAL 30 DAY) AND 

htdocs/lib2/logic/cache.class.php
@@ -419,6 +419,7 @@ class cache
 	}
 	function addRecommendation($nUserId)
 	{
+		// rating_date will be set to NOW() by Insert-trigger
 		sql("INSERT IGNORE INTO `cache_rating` (`cache_id`, `user_id`) VALUES ('&1', '&2')", $this->nCacheId, $nUserId);
 	}
 	function removeRecommendation($nUserId)

htdocs/log.php
@@ -219,7 +219,7 @@
 					// update top-list
 					if ($top_option)
 						if ($top_cache)
-							sql("INSERT IGNORE INTO `cache_rating` (`user_id`, `cache_id`) VALUES('&1', '&2')", $usr['userid'], $cache_id);
+							sql("INSERT IGNORE INTO `cache_rating` (`user_id`, `cache_id`, `rating_date`) VALUES('&1', '&2', '&3')", $usr['userid'], $cache_id, $log_date);
 						else
 							sql("DELETE FROM `cache_rating` WHERE `user_id`='&1' AND `cache_id`='&2'", $usr['userid'], $cache_id);
 

htdocs/util2/cron/modules/rating_tops.class.php
@@ -4,7 +4,7 @@
  *
  *  Unicode Reminder メモ
  *
- *  Publish new geocaches that are marked for timed publish
+ *  Update top rating statistics
  ***************************************************************************/
 
 checkJob(new rating_tops());
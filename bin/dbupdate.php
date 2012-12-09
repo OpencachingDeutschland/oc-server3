c80628e7b09bca83f7207a8cdb021e008b0ca895
ocoliver <o.dietz@nexgo.de>
2012-12-09 14:52:59 +0100

    fixed missing / in rootpath definition

--- bin/dbupdate.php
+++ bin/dbupdate.php
@@ -12,18 +12,18 @@
 	 * Tables must be updated manually, see htdocs/doc/sql/db-changes.txt
 	 */ 
   
-  $rootpath = dirname(__FILE__) . '/..';
-  require($rootpath . '/htdocs/lib/settings.inc.php');
+  $rootpath = dirname(__FILE__) . '/../';
+  require($rootpath . 'htdocs/lib/settings.inc.php');
 	
   echo "importing data.sql ...\n";
-  system('cat ' . $rootpath . '/htdocs/doc/sql/static-data/data.sql | mysql -u' . $dbusername . ' --password=' . $dbpasswd . ' ' . $dbname);
+  system('cat ' . $rootpath . 'htdocs/doc/sql/static-data/data.sql | mysql -u' . $dbusername . ' --password=' . $dbpasswd . ' ' . $dbname);
 
   echo "importing triggers ...\n";
-  chdir ($rootpath . '/htdocs/doc/sql/stored-proc');
+  chdir ($rootpath . 'htdocs/doc/sql/stored-proc');
   system('php maintain.php');
 
   echo "resettings webcache ...\n";
-  chdir ($rootpath . "/bin");
+  chdir ($rootpath . "bin");
   system('php clear-webcache.php');
   
 ?>

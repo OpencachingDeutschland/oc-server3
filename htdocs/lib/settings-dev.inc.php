OpencachingDeutschland/oc-server3

commit d598648f659e1c69f1ba051143f61b90021f4c35

Use config2/locale.inc.php instead of own definition in lib/settings.inc.php

htdocs/lib/settings-dev.inc.php
@@ -137,95 +137,8 @@
   // see config2/settings-dist.inc.php
 	$opt['template']['default']['locale'] = 'DE';      // may be overwritten by $opt['domain'][...]['locale']
 
-	$opt['template']['locales']['DE']['show'] = true;
-	$opt['template']['locales']['DE']['flag'] = 'images/flag/DE.gif';
-	$opt['template']['locales']['DE']['name'] = 'Deutsch';
-	$opt['template']['locales']['FR']['show'] = true;
-	$opt['template']['locales']['FR']['flag'] = 'images/flag/FR.gif';
-	$opt['template']['locales']['FR']['name'] = 'Françs';
-	$opt['template']['locales']['NL']['show'] = true;
-	$opt['template']['locales']['NL']['flag'] = 'images/flag/NL.gif';
-	$opt['template']['locales']['NL']['name'] = 'Nederlands';
-	$opt['template']['locales']['EN']['show'] = true;
-	$opt['template']['locales']['EN']['flag'] = 'images/flag/EN.gif';
-	$opt['template']['locales']['EN']['name'] = 'English';
-	$opt['template']['locales']['PL']['show'] = true;
-	$opt['template']['locales']['PL']['flag'] = 'images/flag/PL.gif';
-	$opt['template']['locales']['PL']['name'] = 'Polski';
-	$opt['template']['locales']['IT']['show'] = true;
-	$opt['template']['locales']['IT']['flag'] = 'images/flag/IT.gif';
-	$opt['template']['locales']['IT']['name'] = 'Italiano';
-	$opt['template']['locales']['RU']['show'] = true;
-	$opt['template']['locales']['RU']['flag'] = 'images/flag/RU.gif';
-	$opt['template']['locales']['RU']['name'] = '.......';
-	$opt['template']['locales']['ES']['show'] = true;
-	$opt['template']['locales']['ES']['flag'] = 'images/flag/ES.png';
-	$opt['template']['locales']['ES']['name'] = 'Espa&ntilde;ol';
-
-	$opt['locale']['EN']['locales'] = array('en_US.utf8', 'en_US', 'en');
-	$opt['locale']['EN']['format']['date'] = '%x';
-	$opt['locale']['EN']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['EN']['format']['datetime'] = '%x %I:%M %p';
-	$opt['locale']['EN']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['EN']['format']['time'] = '%I:%M %p';
-	$opt['locale']['EN']['format']['timesec'] = '%X';
-
-	$opt['locale']['DE']['locales'] = array('de_DE.utf8', 'de_DE@euro', 'de_DE', 'de', 'ge');
-	$opt['locale']['DE']['format']['date'] = '%x';
-	$opt['locale']['DE']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['DE']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['DE']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['DE']['format']['time'] = '%H:%M';
-	$opt['locale']['DE']['format']['timesec'] = '%X';
-
-	$opt['locale']['PL']['locales'] = array('pl_PL.utf8', 'pl_PL', 'pl');
-	$opt['locale']['PL']['format']['date'] = '%x';
-	$opt['locale']['PL']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['PL']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['PL']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['PL']['format']['time'] = '%H:%M';
-	$opt['locale']['PL']['format']['timesec'] = '%X';
-
-	$opt['locale']['NL']['locales'] = array('nl_NL.utf8', 'nl_NL', 'nl');
-	$opt['locale']['NL']['format']['date'] = '%x';
-	$opt['locale']['NL']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['NL']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['NL']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['NL']['format']['time'] = '%H:%M';
-	$opt['locale']['NL']['format']['timesec'] = '%X';
-
-	$opt['locale']['IT']['locales'] = array('it_IT.utf8', 'it_IT', 'it');
-	$opt['locale']['IT']['format']['date'] = '%x';
-	$opt['locale']['IT']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['IT']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['IT']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['IT']['format']['time'] = '%H:%M';
-	$opt['locale']['IT']['format']['timesec'] = '%X';
-
-	$opt['locale']['FR']['locales'] = array('fr_FR.utf8', 'fr_FR@euro', 'fr_FR', 'french', 'fr');
-	$opt['locale']['FR']['format']['date'] = '%x';
-	$opt['locale']['FR']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['FR']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['FR']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['FR']['format']['time'] = '%H:%M';
-	$opt['locale']['FR']['format']['timesec'] = '%X';
-
-	$opt['locale']['RU']['format']['date'] = '%x';
-	$opt['locale']['RU']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['RU']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['RU']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['RU']['format']['time'] = '%H:%M';
-	$opt['locale']['RU']['format']['timesec'] = '%X';
-	$opt['locale']['RU']['country'] = 'RU';
-
-	$opt['locale']['ES']['locales'] = array('es_ES.utf8', 'es_ES', 'es');
-	$opt['locale']['ES']['format']['date'] = '%x';
-	$opt['locale']['ES']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['ES']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['ES']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['ES']['format']['time'] = '%H:%M';
-	$opt['locale']['ES']['format']['timesec'] = '%X';
-	$opt['locale']['ES']['country'] = 'ES';
+  // include all locale settings
+  require_once($rootpath . 'config2/locale.inc.php');
 
 	/* Sponsoring advertisements
 	 * (plain HTML)

htdocs/config2/settings-dev.inc.php
@@ -130,22 +130,10 @@
 	// other settings
 	$opt['logic']['enableHTMLInUserDescription'] = false;
 	$opt['page']['showdonations'] = true;
-
-	// Flag paths
-	$opt['template']['locales']['DE']['flag'] = 'images/flag/DE.gif';
-	$opt['template']['locales']['FR']['flag'] = 'images/flag/FR.gif';
-	$opt['template']['locales']['NL']['flag'] = 'images/flag/NL.gif';
-	$opt['template']['locales']['EN']['flag'] = 'images/flag/EN.gif';
-	$opt['template']['locales']['PL']['flag'] = 'images/flag/PL.gif';
-	$opt['template']['locales']['IT']['flag'] = 'images/flag/IT.gif';
-	$opt['template']['locales']['RU']['flag'] = 'images/flag/RU.gif';
-	$opt['template']['locales']['ES']['flag'] = 'images/flag/ES.png';
-	$opt['template']['locales']['JA']['flag'] = 'images/flag/JP.gif';
 	
 	$opt['template']['locales']['SV']['show'] = false;
 	$opt['template']['locales']['NO']['show'] = false;
 	$opt['template']['locales']['DA']['show'] = false;
 	$opt['template']['locales']['PT']['show'] = false;
 	$opt['template']['locales']['JA']['show'] = false;
-	
 ?>

htdocs/config2/locale.inc.php
@@ -4,40 +4,48 @@
  *
  *  Unicode Reminder メモ
  *
- *  Default settings for all options in settings.inc.php
+ *  Default settings for all locale options in settings.inc.php
  *  Do not modify this file - use settings.inc.php!
+ *
+ *  ATTENTION: This file is also used in old template system.
+ *             (this means any call to framework functions may be incompatible)
+ *
+ *             Only set the following keys in $opt[]
+ *
+ *                 $opt['template']['locales']
+ *                 $opt['geokrety']['locales']
+ *                 $opt['locale']
  ***************************************************************************/
-
 
 	/* Locale definitions
 	 *
 	 */
 	$opt['template']['locales']['DE']['show'] = true;
-	$opt['template']['locales']['DE']['flag'] = 'images/flags/de.gif';
+	$opt['template']['locales']['DE']['flag'] = 'images/flag/DE.gif';
 	$opt['template']['locales']['DE']['name'] = 'Deutsch';
 	$opt['template']['locales']['FR']['show'] = true;
-	$opt['template']['locales']['FR']['flag'] = 'images/flags/fr.gif';
+	$opt['template']['locales']['FR']['flag'] = 'images/flag/FR.gif';
 	$opt['template']['locales']['FR']['name'] = 'Français';
 	$opt['template']['locales']['NL']['show'] = true;
-	$opt['template']['locales']['NL']['flag'] = 'images/flags/nl.gif';
+	$opt['template']['locales']['NL']['flag'] = 'images/flag/NL.gif';
 	$opt['template']['locales']['NL']['name'] = 'Nederlands';
 	$opt['template']['locales']['EN']['show'] = true;
-	$opt['template']['locales']['EN']['flag'] = 'images/flags/en.gif';
+	$opt['template']['locales']['EN']['flag'] = 'images/flag/EN.gif';
 	$opt['template']['locales']['EN']['name'] = 'English';
 	$opt['template']['locales']['PL']['show'] = true;
-	$opt['template']['locales']['PL']['flag'] = 'images/flags/pl.gif';
+	$opt['template']['locales']['PL']['flag'] = 'images/flag/PL.gif';
 	$opt['template']['locales']['PL']['name'] = 'Polski';
 	$opt['template']['locales']['IT']['show'] = true;
-	$opt['template']['locales']['IT']['flag'] = 'images/flags/it.gif';
+	$opt['template']['locales']['IT']['flag'] = 'images/flag/IT.gif';
 	$opt['template']['locales']['IT']['name'] = 'Italiano';
 	$opt['template']['locales']['RU']['show'] = true;
-	$opt['template']['locales']['RU']['flag'] = 'images/flags/ru.gif';
+	$opt['template']['locales']['RU']['flag'] = 'images/flag/RU.gif';
 	$opt['template']['locales']['RU']['name'] = 'Русский';
 	$opt['template']['locales']['ES']['show'] = true;
-	$opt['template']['locales']['ES']['flag'] = 'images/flags/es.gif';
+	$opt['template']['locales']['ES']['flag'] = 'images/flag/ES.png';
 	$opt['template']['locales']['ES']['name'] = 'Español';
 	$opt['template']['locales']['SV']['show'] = true; // sv_SE
-	$opt['template']['locales']['SV']['flag'] = 'images/flags/se.gif';
+	$opt['template']['locales']['SV']['flag'] = 'images/flag/SE.png';
 	$opt['template']['locales']['SV']['name'] = 'Svenska';
 	$opt['template']['locales']['NO']['show'] = true; // no_NO
 	$opt['template']['locales']['NO']['flag'] = 'images/flags/no.gif';
@@ -49,7 +57,7 @@
 	$opt['template']['locales']['PT']['flag'] = 'images/flags/pt.gif';
 	$opt['template']['locales']['PT']['name'] = 'Portuguesa';
 	$opt['template']['locales']['JA']['show'] = true; // ja_JP
-	$opt['template']['locales']['JA']['flag'] = 'images/flags/jp.gif';
+	$opt['template']['locales']['JA']['flag'] = 'images/flag/JP.gif';
 	$opt['template']['locales']['JA']['name'] = '日本語';
 
 	// geokrety language key association

htdocs/lib/settings-dist.inc.php
@@ -145,106 +145,14 @@
 	$opt['template']['default']['locale'] = 'DE';      // may be overwritten by $opt['domain'][...]['locale']
 	$opt['template']['default']['country'] = 'DE';     // may be overwritten by $opt['domain'][...]['country']
 
-	$opt['template']['locales']['DE']['show'] = true;
-	$opt['template']['locales']['DE']['flag'] = 'images/flag/DE.gif';
-	$opt['template']['locales']['DE']['name'] = 'Deutsch';
-	$opt['template']['locales']['FR']['show'] = true;
-	$opt['template']['locales']['FR']['flag'] = 'images/flag/FR.gif';
-	$opt['template']['locales']['FR']['name'] = 'Français';
-	$opt['template']['locales']['NL']['show'] = true;
-	$opt['template']['locales']['NL']['flag'] = 'images/flag/NL.gif';
-	$opt['template']['locales']['NL']['name'] = 'Nederlands';
-	$opt['template']['locales']['EN']['show'] = true;
-	$opt['template']['locales']['EN']['flag'] = 'images/flag/EN.gif';
-	$opt['template']['locales']['EN']['name'] = 'English';
-	$opt['template']['locales']['PL']['show'] = true;
-	$opt['template']['locales']['PL']['flag'] = 'images/flag/PL.gif';
-	$opt['template']['locales']['PL']['name'] = 'Polski';
-	$opt['template']['locales']['IT']['show'] = true;
-	$opt['template']['locales']['IT']['flag'] = 'images/flag/IT.gif';
-	$opt['template']['locales']['IT']['name'] = 'Italiano';
-	$opt['template']['locales']['RU']['show'] = true;
-	$opt['template']['locales']['RU']['flag'] = 'images/flag/RU.gif';
-	$opt['template']['locales']['RU']['name'] = 'Russian';
-
-	$opt['locale']['EN']['locales'] = array('en_US.utf8', 'en_US', 'en');
-	$opt['locale']['EN']['format']['date'] = '%x';
-	$opt['locale']['EN']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['EN']['format']['datetime'] = '%x %I:%M %p';
-	$opt['locale']['EN']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['EN']['format']['time'] = '%I:%M %p';
-	$opt['locale']['EN']['format']['timesec'] = '%X';
-
-	$opt['locale']['DE']['locales'] = array('de_DE.utf8', 'de_DE@euro', 'de_DE', 'de', 'ge');
-	$opt['locale']['DE']['format']['date'] = '%x';
-	$opt['locale']['DE']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['DE']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['DE']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['DE']['format']['time'] = '%H:%M';
-	$opt['locale']['DE']['format']['timesec'] = '%X';
-	$opt['locale']['DE']['page']['subtitle1'] = 'Geocaching mit Opencaching';
-	$opt['locale']['DE']['page']['subtitle2'] = '';
-
-	$opt['locale']['PL']['locales'] = array('pl_PL.utf8', 'pl_PL', 'pl');
-	$opt['locale']['PL']['format']['date'] = '%x';
-	$opt['locale']['PL']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['PL']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['PL']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['PL']['format']['time'] = '%H:%M';
-	$opt['locale']['PL']['format']['timesec'] = '%X';
-
-	$opt['locale']['NL']['locales'] = array('nl_NL.utf8', 'nl_NL', 'nl');
-	$opt['locale']['NL']['format']['date'] = '%x';
-	$opt['locale']['NL']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['NL']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['NL']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['NL']['format']['time'] = '%H:%M';
-	$opt['locale']['NL']['format']['timesec'] = '%X';
-	$opt['locale']['NL']['page']['subtitle1'] = 'Geocaching met Opencaching';
-	$opt['locale']['NL']['page']['subtitle2'] = '';
-
-	$opt['locale']['IT']['locales'] = array('it_IT.utf8', 'it_IT', 'it');
-	$opt['locale']['IT']['format']['date'] = '%x';
-	$opt['locale']['IT']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['IT']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['IT']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['IT']['format']['time'] = '%H:%M';
-	$opt['locale']['IT']['format']['timesec'] = '%X';
-	$opt['locale']['IT']['page']['subtitle1'] = 'Geocaching con Opencaching';
-	$opt['locale']['IT']['page']['subtitle2'] = '';
-
-	$opt['locale']['FR']['locales'] = array('fr_FR.utf8', 'fr_FR@euro', 'fr_FR', 'french', 'fr');
-	$opt['locale']['FR']['format']['date'] = '%x';
-	$opt['locale']['FR']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['FR']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['FR']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['FR']['format']['time'] = '%H:%M';
-	$opt['locale']['FR']['format']['timesec'] = '%X';
-
-	$opt['locale']['RU']['locales'] = array('ru_RU.utf8', 'ru_RU', 'ru');
-	$opt['locale']['RU']['format']['date'] = '%x';
-	$opt['locale']['RU']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['RU']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['RU']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['RU']['format']['time'] = '%H:%M';
-	$opt['locale']['RU']['format']['timesec'] = '%X';
-
-	$opt['locale']['ES']['locales'] = array('es_ES.utf8', 'es_ES', 'es');
-	$opt['locale']['ES']['format']['date'] = '%x';
-	$opt['locale']['ES']['format']['datelong'] = '%d. %B %Y';
-	$opt['locale']['ES']['format']['datetime'] = '%x %H:%M';
-	$opt['locale']['ES']['format']['datetimesec'] = '%x %X';
-	$opt['locale']['ES']['format']['time'] = '%H:%M';
-	$opt['locale']['ES']['format']['timesec'] = '%X';
-	$opt['locale']['ES']['country'] = 'ES';
-	$opt['locale']['ES']['page']['subtitle1'] = 'Geocaching con Opencaching';
-	$opt['locale']['ES']['page']['subtitle2'] = '';
+  // include all locale settings
+  require_once($rootpath . 'config2/locale.inc.php');
 
 	$opt['page']['title'] = 'OPENCACHING';
 	$opt['page']['subtitle1'] = 'Geocaching with Opencaching';
 	$opt['page']['subtitle2'] = '';
 
-/* Sponsoring advertisements
+  /* Sponsoring advertisements
    * (plain HTML)
    */
 

commit 8a6bb7727a8c1f84106e48d84c5b32da9a9b5973

fixed developer system language menu, was broken by commit d598648f

htdocs/lib/settings-dev.inc.php
@@ -163,4 +163,10 @@
 	// (e.g. xml-interface and mapserver-results)
 	// you can use -1 to use the master (not recommended, because replicated to slaves)
 	$opt['db']['slave']['primary'] = -1;
+
+	$opt['template']['locales']['SV']['show'] = false;
+	$opt['template']['locales']['NO']['show'] = false;
+	$opt['template']['locales']['DA']['show'] = false;
+	$opt['template']['locales']['PT']['show'] = false;
+	$opt['template']['locales']['JA']['show'] = false;
 ?>
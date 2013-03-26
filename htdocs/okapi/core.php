e399434620b8b9e2da8fe9a9f7758bfb563fbced
following <following>
2013-03-26 18:04:43 +0100

    fixed okapi revision

--- htdocs/okapi/core.php
+++ htdocs/okapi/core.php
@@ -778,7 +778,7 @@ class Okapi
 {
 	public static $data_store;
 	public static $server;
-	public static $revision = null; # This gets replaced in automatically deployed packages
+	public static $revision = 567; # This gets replaced in automatically deployed packages
 	private static $okapi_vars = null;
 
 	/** Get a variable stored in okapi_vars. If variable not found, return $default. */

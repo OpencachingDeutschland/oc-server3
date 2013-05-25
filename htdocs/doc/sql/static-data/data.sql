792be0749d26738aaceaaea120e1b531a8dfa006
Nils Bohrs <nils@bohrs.net>
2013-05-25 12:39:38 +0200

    added webchat.tpl and webchat.php for viewing chat/irc embedded in oc-layout, defined MNU_CHAT in data.sql, disabled template caching, moved webchat URL to settings file, removed inclusion of not needed lib2/logic/logpics.inc.php, increased size of chat window and made it configurable via settings file, updates #196

--- htdocs/doc/sql/static-data/data.sql
+++ htdocs/doc/sql/static-data/data.sql
@@ -888,6 +888,7 @@ INSERT INTO `sys_menu` (`id`, `id_string`, `title`, `title_trans_id`, `menustrin
 INSERT INTO `sys_menu` (`id`, `id_string`, `title`, `title_trans_id`, `menustring`, `menustring_trans_id`, `access`, `href`, `visible`, `parent`, `position`, `color`, `sitemap`, `only_if_parent`) VALUES ('93', 'MNU_ADMIN_RESTORE', 'Vandalism', '1880', 'Vandalism', '1880', '0', 'restorecaches.php', '1', '12', '5', '', '0', NULL);
 INSERT INTO `sys_menu` (`id`, `id_string`, `title`, `title_trans_id`, `menustring`, `menustring_trans_id`, `access`, `href`, `visible`, `parent`, `position`, `color`, `sitemap`, `only_if_parent`) VALUES ('94', 'MNU_START_NEWLOGSREST', 'Without Germany', '121', 'Without Germany', '121', '0', 'newlogsrest.php', '1', '7', '1', '', '1', NULL);
 INSERT INTO `sys_menu` (`id`, `id_string`, `title`, `title_trans_id`, `menustring`, `menustring_trans_id`, `access`, `href`, `visible`, `parent`, `position`, `color`, `sitemap`, `only_if_parent`) VALUES ('95', 'MNU_MYPROFILE_OKAPI', 'OKAPI Applications', '2008', 'OKAPI Apps', '2009', '0', '!okapi/apps/', '1', '9', '9', '', '1', NULL);
+INSERT INTO `sys_menu` (`id`, `id_string`, `title`, `title_trans_id`, `menustring`, `menustring_trans_id`, `access`, `href`, `visible`, `parent`, `position`, `color`, `sitemap`, `only_if_parent`) VALUES ('96', 'MNU_CHAT', 'Chat/IRC', '957', 'Chat/IRC', '957', '0', 'webchat.php', '1', '0', '5', '', '1', NULL);
 
 -- Table sys_trans
 SET NAMES 'utf8';

2a6a7b7377fa6634b8fbf33538e15aac64d41597
following <following@online.de>
2013-07-16 13:33:54 +0200

    okapi r879: changed GPX child waypoints order

--- htdocs/okapi/core.php
+++ htdocs/okapi/core.php
@@ -814,7 +814,7 @@ class Okapi
 {
 	public static $data_store;
 	public static $server;
-	public static $revision = 878; # This gets replaced in automatically deployed packages
+	public static $revision = 879; # This gets replaced in automatically deployed packages
 	private static $okapi_vars = null;
 
 	/** Get a variable stored in okapi_vars. If variable not found, return $default. */

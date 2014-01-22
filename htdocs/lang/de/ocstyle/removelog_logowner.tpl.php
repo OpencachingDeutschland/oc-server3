cef2b16374a46a75242aac76132601727f26e558
clickos <thoese@gmail.com>
2014-01-22 12:06:56 +0100

    update #268 : switched to standard-header in templates

--- htdocs/lang/de/ocstyle/removelog_logowner.tpl.php
+++ htdocs/lang/de/ocstyle/removelog_logowner.tpl.php
@@ -19,11 +19,11 @@
 
  ****************************************************************************/
 ?>
+<div class="content2-pagetitle"><img src="lang/de/ocstyle/images/description/22x22-logs.png" style="align: left; margin-right: 10px;" width="22" height="22"/>{t}remove log entry for <a href="viewcache.php?cacheid={cacheid}">{cachename}</a>{/t}</div>
 <form action="removelog.php" method="post" enctype="application/x-www-form-urlencoded" name="removelog_form" dir="ltr">
 <input type="hidden" name="commit" value="1"/>
 <input type="hidden" name="logid" value="{logid}"/>
 <table class="table">
-	<tr><td class="header" colspan="2"><img src="lang/de/ocstyle/images/description/22x22-logs.png" border="0" width="22" height="22" alt="" title="" align="middle" /> <b>{t}remove log entry for <a href="viewcache.php?cacheid={cacheid}">{cachename}</a>{/t}</b></td></tr>
 	<tr><td class="spacer"></td></tr>
 
 	<tr><td colspan="2">{t}Are you sure to remove your log entry?{/t}</td></tr>

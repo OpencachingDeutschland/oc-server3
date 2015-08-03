f57b26c3ef04f00952700f601cbf6bacf80ecedb
following <following@online.de>
2015-08-03 14:36:28 +0200

    OKAPI r1105

--- htdocs/okapi/core.php
+++ htdocs/okapi/core.php
@@ -966,8 +966,8 @@ class Okapi
     public static $server;
 
     /* These two get replaced in automatically deployed packages. */
-    public static $version_number = 1104;
-    public static $git_revision = 'a6bcf018d7b19b0f0a346a22a41dd19204d7f51a';
+    public static $version_number = 1105;
+    public static $git_revision = '69bb1a7be72b7a52621532626e5c6813fb26e354';
 
     private static $okapi_vars = null;
 
@@ -976,7 +976,7 @@ class Okapi
     {
         /* If we're on Linux, then we'll use a system function for that. */
 
-        if (@file_exists("/proc/sys/kernel/random/uuid")) {
+        if (file_exists("/proc/sys/kernel/random/uuid")) {
             return trim(file_get_contents("/proc/sys/kernel/random/uuid"));
         }
 

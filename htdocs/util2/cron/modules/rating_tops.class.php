0e0a4eaa968872f96f63798b53ed845d87dd3a88
following <following@online.de>
2015-08-07 17:08:20 +0200

    always prefer user-supplied cache country; updates #82

--- htdocs/util2/cron/modules/rating_tops.class.php
+++ htdocs/util2/cron/modules/rating_tops.class.php
@@ -26,10 +26,17 @@ class rating_tops
 		sql("CREATE TEMPORARY TABLE &topRatings (`cache_id` INT(11) PRIMARY KEY, `ratings` INT(11)) ENGINE=MEMORY");
 		sql("CREATE TEMPORARY TABLE &topResult (`idx` INT(11), `cache_id` INT(11) PRIMARY KEY, `ratings` INT(11), `founds` INT(11)) ENGINE=MEMORY");
 
-		$rsAdm1 = sql('SELECT SQL_BUFFER_RESULT SQL_SMALL_RESULT DISTINCT `adm1`, `code1` FROM `cache_location` WHERE NOT ISNULL(`adm1`) ORDER BY `adm1` ASC');
-		while ($rAdm1 = sql_fetch_assoc($rsAdm1))
+		$rsCountry = sql('SELECT SQL_BUFFER_RESULT SQL_SMALL_RESULT DISTINCT `country` FROM `caches`');
+		while ($rCountry = sql_fetch_assoc($rsCountry))
 		{
-			$rsAdm3 = sql("SELECT SQL_BUFFER_RESULT SQL_SMALL_RESULT DISTINCT `adm3` FROM `cache_location` WHERE `code1`='&1' ORDER BY `adm3` ASC", $rAdm1['code1']);
+			$rsAdm3 = sql("
+				SELECT SQL_BUFFER_RESULT SQL_SMALL_RESULT DISTINCT 
+				  IF(`cache_location`.`code1`=`caches`.`country`,`cache_location`.`code3`,NULL) `code3`
+				FROM `caches`
+				LEFT JOIN `cache_location` ON `caches`.`cache_id`=`cache_location`.`cache_id`
+				WHERE `caches`.`country`='&1'",
+				$rCountry['country']);
+
 			while ($rAdm3 = sql_fetch_assoc($rsAdm3))
 			{
 				sql("TRUNCATE TABLE &topLocationCaches");
@@ -37,12 +44,33 @@ class rating_tops
 				sql("TRUNCATE TABLE &topResult");
 
 				// Alle Caches für diese Gruppe finden
-				if ($rAdm3['adm3'] == null)
-					sql("INSERT INTO &topLocationCaches (`cache_id`) SELECT `caches`.`cache_id` FROM `cache_location` INNER JOIN `caches` ON `caches`.`cache_id`=`cache_location`.`cache_id` LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id` WHERE IFNULL(`stat_caches`.`toprating`,0)>0 AND `cache_location`.`adm1`='&1' AND ISNULL(`cache_location`.`adm3`) AND `caches`.`status`=1", $rAdm1['adm1']);
+				if ($rAdm3['code3'] == null)
+					sql("
+						INSERT INTO &topLocationCaches (`cache_id`)
+							SELECT `caches`.`cache_id`
+							FROM `cache_location`
+							INNER JOIN `caches` ON `caches`.`cache_id`=`cache_location`.`cache_id`
+							LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id`
+							WHERE IFNULL(`stat_caches`.`toprating`,0)>0 AND `cache_location`.`code1`='&1' AND ISNULL(`cache_location`.`code3`) AND `caches`.`status`=1",
+							$rCountry['country']);
 				else
-					sql("INSERT INTO &topLocationCaches (`cache_id`) SELECT `caches`.`cache_id` FROM `cache_location` INNER JOIN `caches` ON `caches`.`cache_id`=`cache_location`.`cache_id` LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id` WHERE IFNULL(`stat_caches`.`toprating`,0)>0 AND `cache_location`.`adm1`='&1' AND `cache_location`.`adm3`='&2' AND `caches`.`status`=1", $rAdm1['adm1'], $rAdm3['adm3']);
+					sql("
+						INSERT INTO &topLocationCaches (`cache_id`)
+						SELECT `caches`.`cache_id`
+						FROM `cache_location`
+						INNER JOIN `caches` ON `caches`.`cache_id`=`cache_location`.`cache_id`
+						LEFT JOIN `stat_caches` ON `caches`.`cache_id`=`stat_caches`.`cache_id`
+						WHERE IFNULL(`stat_caches`.`toprating`,0)>0 AND `cache_location`.`code1`='&1' AND `cache_location`.`code3`='&2' AND `caches`.`status`=1",
+						$rCountry['country'], $rAdm3['code3']);
 
-				sql("INSERT INTO &topRatings (`cache_id`, `ratings`) SELECT `cache_rating`.`cache_id`, COUNT(`cache_rating`.`cache_id`) AS `ratings` FROM `cache_rating` INNER JOIN &topLocationCaches ON `cache_rating`.`cache_id`=&topLocationCaches.`cache_id` INNER JOIN `caches` ON `cache_rating`.`cache_id`=`caches`.`cache_id` WHERE `cache_rating`.`user_id`!=`caches`.`user_id` GROUP BY `cache_rating`.`cache_id`");
+				sql("
+					INSERT INTO &topRatings (`cache_id`, `ratings`)
+					SELECT `cache_rating`.`cache_id`, COUNT(`cache_rating`.`cache_id`) AS `ratings`
+					FROM `cache_rating`
+					INNER JOIN &topLocationCaches ON `cache_rating`.`cache_id`=&topLocationCaches.`cache_id`
+					INNER JOIN `caches` ON `cache_rating`.`cache_id`=`caches`.`cache_id`
+					WHERE `cache_rating`.`user_id`!=`caches`.`user_id`
+					GROUP BY `cache_rating`.`cache_id`");
 
 				sql("INSERT INTO &topResult (`idx`, `cache_id`, `ratings`, `founds`) 
 				     SELECT SQL_SMALL_RESULT (&topRatings.`ratings`+1)*(&topRatings.`ratings`+1)/(IFNULL(`stat_caches`.`found`, 0)/10+1)*100 AS `idx`, 
@@ -68,7 +96,7 @@ class rating_tops
 			}
 			sql_free_result($rsAdm3);
 		}
-		sql_free_result($rsAdm1);
+		sql_free_result($rsCountry);
 
 		sql_drop_temp_table('topLocationCaches');
 		sql_drop_temp_table('topRatings');

c323d09d07f9853b0ce49518bf891a2c18be163f
following <following@online.de>
2016-02-02 00:05:23 +0100

    improved httpd detection

--- htdocs/config2/settings-dist-common.inc.php
+++ htdocs/config2/settings-dist-common.inc.php
@@ -297,7 +297,9 @@ function cronjobs_enabled()
 {
 	global $opt;
 	return $opt['cron']['debug'] ||
-	       @file_get_contents($opt['page']['absolute_http_url'] . 'register.php') !== false;
+	       // @file_get_contents($opt['page']['absolute_http_url'] . 'register.php') !== false;
+	       // -- This will not work if the server is access protected.
+	       exec('ps -C httpd|grep httpd');
 }
 
 ?>

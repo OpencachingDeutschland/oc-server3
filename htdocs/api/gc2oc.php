b0f1f2f74b6e5a9f01f6482fb199466b067f22df
following <following@online.de>
2016-03-10 20:14:48 +0100

    added caching and state info to gc2oc wp api

--- htdocs/api/gc2oc.php
+++ htdocs/api/gc2oc.php
@@ -5,12 +5,18 @@
  *  Unicode Reminder メモ
  *
  *  returns GC -> OC waypoint translation table, if available
+ *
+ *  file format: gcwp,ocwp,checked
+ *  checked = 1 if the GC waypoint has been verified,
+ *            0 if it is directly taken from an OC cache listing.
+ *
  ***************************************************************************/
 
 	$opt['rootpath'] = '../';
 	require($opt['rootpath'] . 'lib2/web.inc.php');
 
-	header('Content-type: text/plain; charset=utf-8');
+	header('Content-type: application/x-gzip');
+	header('Content-Disposition: attachment; filename=gc2oc.gz');
 
 	/*
 	 * caches.wp_gc_maintained is intended for map and search filtering and
@@ -19,8 +25,8 @@
 	 * If a better external source is available, we can use data from there.
 	 *
 	 * This may be refined by combining different sources and/or internal data.
-	 * Also, it should be optimized by caching the list contents and allowing
-	 * to request a single GC code or a list of GC codes.
+	 * Also, it may be optimized by allowing to request a single GC code or a
+	 * list of GC codes.
 	 *
 	 * Note that it is not possible to create a 100% reliable translation table.
 	 * There are many wrong GC wps in listings, and maintained tables always
@@ -28,6 +34,22 @@
 	 */
 
 	if ($opt['cron']['gcwp']['fulllist'])
-		echo @file_get_contents($opt['cron']['gcwp']['fulllist']);
+	{
+		$gzipped_data = '';
+		$cachefile = '../cache2/gc2oc.gz';
+		if (!file_exists($cachefile) || time() - filemtime($cachefile) > 3600)
+		{
+			$gc2oc = file_get_contents($opt['cron']['gcwp']['fulllist']);
+			if ($gc2oc)
+			{
+				$gzipped_data = gzencode($gc2oc);
+				file_put_contents($cachefile, $gzipped_data);
+			}
+		}
 
+		if (!$gzipped_data && file_exists($cachefile))
+			$gzipped_data = file_get_contents($cachefile);
+
+		echo $gzipped_data;
+	}
 ?>

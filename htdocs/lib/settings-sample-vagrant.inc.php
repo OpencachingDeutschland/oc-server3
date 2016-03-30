3dc205ce3441fc770be247ae82bc953006ad2951
Thomas Eiling <teiling88@gmail.com>
2016-03-30 20:44:02 +0200

    adding vagrant file and provision.sh file

---
+++ htdocs/lib/settings-sample-vagrant.inc.php
@@ -0,0 +1,21 @@
+<?php
+/***************************************************************************
+ *  You may enter special or testing settings for your developer maching here
+ *
+ *  Unicode Reminder メモ
+ ***************************************************************************/
+// installation paths
+$dev_basepath = '/var/www/html/';
+$dev_codepath = '';
+$dev_baseurl = 'http://local.opencaching.de';
+// database acccess
+$dbserver = 'localhost';
+$dbusername = 'root';
+$dbpasswd = '';
+$dbpconnect = false;
+// database names
+$dbname = 'opencaching';
+$tmpdbname = 'octmp';   // empty db with CREATE and DROP privileges
+// common developer system settings
+require("settings-dev.inc.php");
+$sql_errormail = 'root';

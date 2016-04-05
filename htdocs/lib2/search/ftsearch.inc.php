2ae40e05816f30651cc32cbd005a672b82145ec6
Thomas Eiling <teiling88@gmail.com>
2016-04-05 16:50:12 +0200

    new format some files of project source

--- htdocs/lib2/search/ftsearch.inc.php
+++ htdocs/lib2/search/ftsearch.inc.php
@@ -1,61 +1,160 @@
 <?php
 /****************************************************************************
-		For license information see doc/license.txt
-
-		Unicode Reminder メモ
-
-		functions for the full text search-engine
-
+ * For license information see doc/license.txt
+ *
+ * Unicode Reminder メモ
+ *
+ * functions for the full text search-engine
  ****************************************************************************/
 
 /* begin conversion rules */
 
-	$ftsearch_simplerules[] = array('qu', 'k');
-	$ftsearch_simplerules[] = array('ts', 'z');
-	$ftsearch_simplerules[] = array('tz', 'z');
-	$ftsearch_simplerules[] = array('alp', 'alb');
-	$ftsearch_simplerules[] = array('y', 'i');
-	$ftsearch_simplerules[] = array('ai', 'ei');
-	$ftsearch_simplerules[] = array('ou', 'u');
-	$ftsearch_simplerules[] = array('th', 't');
-	$ftsearch_simplerules[] = array('ph', 'f');
-	$ftsearch_simplerules[] = array('oh', 'o');
-	$ftsearch_simplerules[] = array('ah', 'a');
-	$ftsearch_simplerules[] = array('eh', 'e');
-	$ftsearch_simplerules[] = array('aux', 'o');
-	$ftsearch_simplerules[] = array('eau', 'o');
-	$ftsearch_simplerules[] = array('eux', 'oe');
-	$ftsearch_simplerules[] = array('^ch', 'sch');
-	$ftsearch_simplerules[] = array('ck', 'k');
-	$ftsearch_simplerules[] = array('ie', 'i');
-	$ftsearch_simplerules[] = array('ih', 'i');
-	$ftsearch_simplerules[] = array('ent', 'end');
-	$ftsearch_simplerules[] = array('uh', 'u');
-	$ftsearch_simplerules[] = array('sh', 'sch');
-	$ftsearch_simplerules[] = array('ver', 'wer');
-	$ftsearch_simplerules[] = array('dt', 't');
-	$ftsearch_simplerules[] = array('hard', 'hart');
-	$ftsearch_simplerules[] = array('egg', 'ek');
-	$ftsearch_simplerules[] = array('eg', 'ek');
-	$ftsearch_simplerules[] = array('cr', 'kr');
-	$ftsearch_simplerules[] = array('ca', 'ka');
-	$ftsearch_simplerules[] = array('ce', 'ze');
-	$ftsearch_simplerules[] = array('x', 'ks');
-	$ftsearch_simplerules[] = array('ve', 'we');
-	$ftsearch_simplerules[] = array('va', 'wa');
+$ftsearch_simplerules[] = [
+	'qu',
+	'k'
+];
+$ftsearch_simplerules[] = [
+	'ts',
+	'z'
+];
+$ftsearch_simplerules[] = [
+	'tz',
+	'z'
+];
+$ftsearch_simplerules[] = [
+	'alp',
+	'alb'
+];
+$ftsearch_simplerules[] = [
+	'y',
+	'i'
+];
+$ftsearch_simplerules[] = [
+	'ai',
+	'ei'
+];
+$ftsearch_simplerules[] = [
+	'ou',
+	'u'
+];
+$ftsearch_simplerules[] = [
+	'th',
+	't'
+];
+$ftsearch_simplerules[] = [
+	'ph',
+	'f'
+];
+$ftsearch_simplerules[] = [
+	'oh',
+	'o'
+];
+$ftsearch_simplerules[] = [
+	'ah',
+	'a'
+];
+$ftsearch_simplerules[] = [
+	'eh',
+	'e'
+];
+$ftsearch_simplerules[] = [
+	'aux',
+	'o'
+];
+$ftsearch_simplerules[] = [
+	'eau',
+	'o'
+];
+$ftsearch_simplerules[] = [
+	'eux',
+	'oe'
+];
+$ftsearch_simplerules[] = [
+	'^ch',
+	'sch'
+];
+$ftsearch_simplerules[] = [
+	'ck',
+	'k'
+];
+$ftsearch_simplerules[] = [
+	'ie',
+	'i'
+];
+$ftsearch_simplerules[] = [
+	'ih',
+	'i'
+];
+$ftsearch_simplerules[] = [
+	'ent',
+	'end'
+];
+$ftsearch_simplerules[] = [
+	'uh',
+	'u'
+];
+$ftsearch_simplerules[] = [
+	'sh',
+	'sch'
+];
+$ftsearch_simplerules[] = [
+	'ver',
+	'wer'
+];
+$ftsearch_simplerules[] = [
+	'dt',
+	't'
+];
+$ftsearch_simplerules[] = [
+	'hard',
+	'hart'
+];
+$ftsearch_simplerules[] = [
+	'egg',
+	'ek'
+];
+$ftsearch_simplerules[] = [
+	'eg',
+	'ek'
+];
+$ftsearch_simplerules[] = [
+	'cr',
+	'kr'
+];
+$ftsearch_simplerules[] = [
+	'ca',
+	'ka'
+];
+$ftsearch_simplerules[] = [
+	'ce',
+	'ze'
+];
+$ftsearch_simplerules[] = [
+	'x',
+	'ks'
+];
+$ftsearch_simplerules[] = [
+	've',
+	'we'
+];
+$ftsearch_simplerules[] = [
+	'va',
+	'wa'
+];
 
 /* end conversion rules */
 
 function ftsearch_hash(&$str)
 {
 	$astr = ftsearch_split($str, true);
-	foreach ($astr AS $k => $s)
-	{
-		if (strlen($s) > 2)
+	foreach ($astr AS $k => $s) {
+		if (strlen($s) > 2) {
 			$astr[$k] = sprintf("%u", crc32($s));
-		else
+		} else {
 			unset($astr[$k]);
+		}
 	}
+
 	return $astr;
 }
 
@@ -80,8 +179,7 @@ function ftsearch_split(&$str, $simple)
 	$str = mb_ereg_replace("\r", ' ', $str);
 
 	$ostr = '';
-	while ($ostr != $str)
-	{
+	while ($ostr != $str) {
 		$ostr = $str;
 		$str = mb_ereg_replace('  ', ' ', $str);
 	}
@@ -90,18 +188,18 @@ function ftsearch_split(&$str, $simple)
 	$str = '';
 
 	ftsearch_load_ignores();
-	for ($i = count($astr) - 1; $i >= 0; $i--)
-	{
+	for ($i = count($astr) - 1; $i >= 0; $i --) {
 		// ignore?
-		if (array_search(mb_strtolower($astr[$i]), $ftsearch_ignores) !== false)
+		if (array_search(mb_strtolower($astr[$i]), $ftsearch_ignores) !== false) {
 			unset($astr[$i]);
-		else
-		{
-			if ($simple)
+		} else {
+			if ($simple) {
 				$astr[$i] = ftsearch_text2simple($astr[$i]);
+			}
 
-			if ($astr[$i] == '')
+			if ($astr[$i] == '') {
 				unset($astr[$i]);
+			}
 		}
 	}
 
@@ -113,13 +211,13 @@ function ftsearch_load_ignores()
 	global $ftsearch_ignores;
 	global $ftsearch_ignores_loaded;
 
-	if ($ftsearch_ignores_loaded != true)
-	{
+	if ($ftsearch_ignores_loaded != true) {
 		$ftsearch_ignores = array();
 
 		$rs = sql('SELECT `word` FROM `search_ignore`');
-		while ($r = sql_fetch_assoc($rs))
+		while ($r = sql_fetch_assoc($rs)) {
 			$ftsearch_ignores[] = $r['word'];
+		}
 		sql_free_result($rs);
 
 		$ftsearch_ignores_loaded = true;
@@ -134,17 +232,14 @@ function ftsearch_text2simple($str)
 	$str = ftsearch_text2sort($str);
 
 	// regeln anwenden
-	foreach ($ftsearch_simplerules AS $rule)
-	{
+	foreach ($ftsearch_simplerules AS $rule) {
 		$str = mb_ereg_replace($rule[0], $rule[1], $str);
 	}
 
 	// doppelte chars ersetzen
-	for ($c = ord('a'); $c <= ord('z'); $c++)
-	{
+	for ($c = ord('a'); $c <= ord('z'); $c ++) {
 		$old_str = '';
-		while ($old_str != $str)
-		{
+		while ($old_str != $str) {
 			$old_str = $str;
 			$str = mb_ereg_replace(chr($c) . chr($c), chr($c), $str);
 		}
@@ -160,18 +255,18 @@ function ftsearch_text2sort($str)
 	$str = mb_strtolower($str);
 
 	// deutsches
-  $str = mb_ereg_replace('ä', 'ae', $str);
+	$str = mb_ereg_replace('ä', 'ae', $str);
 	$str = mb_ereg_replace('ö', 'oe', $str);
 	$str = mb_ereg_replace('ü', 'ue', $str);
-  $str = mb_ereg_replace('Ä', 'ae', $str);
+	$str = mb_ereg_replace('Ä', 'ae', $str);
 	$str = mb_ereg_replace('Ö', 'oe', $str);
 	$str = mb_ereg_replace('Ü', 'ue', $str);
 	$str = mb_ereg_replace('ß', 'ss', $str);
 
-  // akzente usw.
-  $str = mb_ereg_replace('à', 'a', $str);
-  $str = mb_ereg_replace('á', 'a', $str);
-  $str = mb_ereg_replace('â', 'a', $str);
+	// akzente usw.
+	$str = mb_ereg_replace('à', 'a', $str);
+	$str = mb_ereg_replace('á', 'a', $str);
+	$str = mb_ereg_replace('â', 'a', $str);
 	$str = mb_ereg_replace('è', 'e', $str);
 	$str = mb_ereg_replace('é', 'e', $str);
 	$str = mb_ereg_replace('ë', 'e', $str);
@@ -217,17 +312,33 @@ function ftsearch_refresh()
 
 function ftsearch_refresh_all_caches()
 {
-	$rs = sql('SELECT `caches`.`cache_id` FROM `caches` LEFT JOIN `search_index_times` ON `caches`.`cache_id`=`search_index_times`.`object_id` AND 2=`search_index_times`.`object_type` WHERE `caches`.`status`!=5 AND ISNULL(`search_index_times`.`object_id`) UNION DISTINCT SELECT `caches`.`cache_id` FROM `caches` INNER JOIN `search_index_times` ON `search_index_times`.`object_type`=2 AND `caches`.`cache_id`=`search_index_times`.`object_id` WHERE `caches`.`last_modified`>`search_index_times`.`last_refresh` AND `caches`.`status`!=5');
-	while ($r = sql_fetch_assoc($rs))
+	$rs = sql(
+		'SELECT `caches`.`cache_id`
+		 FROM `caches`
+		 LEFT JOIN `search_index_times`
+		     ON `caches`.`cache_id`=`search_index_times`.`object_id` 
+             AND 2 =`search_index_times`.`object_type`
+		 WHERE `caches`.`status`!= 5
+		 AND ISNULL(`search_index_times`.`object_id`) 
+		 UNION DISTINCT 
+		 SELECT `caches`.`cache_id` 
+		 FROM `caches`
+		 INNER JOIN `search_index_times`
+		     ON `search_index_times`.`object_type`=2 
+             AND `caches`.`cache_id`=`search_index_times`.`object_id`
+         WHERE `caches`.`last_modified` > `search_index_times`.`last_refresh`
+             AND `caches`.`status`!=5'
+	);
+	while ($r = sql_fetch_assoc($rs)) {
 		ftsearch_refresh_cache($r['cache_id']);
+	}
 	sql_free_result($rs);
 }
 
 function ftsearch_refresh_cache($cache_id)
 {
 	$rs = sql("SELECT `name`, `last_modified` FROM `caches` WHERE `cache_id`='&1'", $cache_id);
-	if ($r = sql_fetch_assoc($rs))
-	{
+	if ($r = sql_fetch_assoc($rs)) {
 		ftsearch_set_entries(2, $cache_id, $cache_id, $r['name'], $r['last_modified']);
 	}
 	sql_free_result($rs);
@@ -235,17 +346,46 @@ function ftsearch_refresh_cache($cache_id)
 
 function ftsearch_refresh_all_cache_desc()
 {
-	$rs = sql('SELECT `cache_desc`.`id` FROM `cache_desc` INNER JOIN `caches` ON `caches`.`cache_id`=`cache_desc`.`cache_id` LEFT JOIN `search_index_times` ON `cache_desc`.`id`=`search_index_times`.`object_id` AND 3=`search_index_times`.`object_type` WHERE `caches`.`status`!=5 AND ISNULL(`search_index_times`.`object_id`) UNION DISTINCT SELECT `cache_desc`.`id` FROM `cache_desc` INNER JOIN `caches` ON `caches`.`cache_id`=`cache_desc`.`cache_id` INNER JOIN `search_index_times` ON `search_index_times`.`object_type`=3 AND `cache_desc`.`id`=`search_index_times`.`object_id` WHERE `cache_desc`.`last_modified`>`search_index_times`.`last_refresh` AND `caches`.`status`!=5');
-	while ($r = sql_fetch_assoc($rs))
-		ftsearch_refresh_cache_desc($r['id']);
+	$rs = sql(
+		'SELECT `cache_desc`.`id`
+		 FROM `cache_desc`
+		 INNER JOIN `caches`
+		     ON `caches`.`cache_id`=`cache_desc`.`cache_id`
+         LEFT JOIN `search_index_times`
+             ON `cache_desc`.`id`=`search_index_times`.`object_id` 
+             AND 3=`search_index_times`.`object_type`
+         WHERE `caches`.`status`!= 5
+             AND ISNULL(`search_index_times`.`object_id`) 
+         UNION DISTINCT 
+         SELECT `cache_desc`.`id`
+         FROM `cache_desc` 
+         INNER JOIN `caches`
+             ON `caches`.`cache_id`=`cache_desc`.`cache_id` 
+         INNER JOIN `search_index_times`
+             ON `search_index_times`.`object_type` = 3 
+             AND `cache_desc`.`id`=`search_index_times`.`object_id`
+         WHERE `cache_desc`.`last_modified`>`search_index_times`.`last_refresh`
+             AND `caches`.`status`!=5'
+    );
+	while ($r = sql_fetch_assoc($rs)) {
+        ftsearch_refresh_cache_desc($r['id']);
+    }
 	sql_free_result($rs);
 }
 
 function ftsearch_refresh_cache_desc($id)
 {
-	$rs = sql("SELECT `cache_id`, `desc`, `last_modified` FROM `cache_desc` WHERE `id`='&1'", $id);
-	if ($r = sql_fetch_assoc($rs))
-	{
+	$rs = sql(
+        "
+        SELECT
+          `cache_id`,
+          `desc`,
+          `last_modified`
+        FROM `cache_desc`
+        WHERE `id`='&1'",
+        $id
+    );
+	if ($r = sql_fetch_assoc($rs)) {
 		$r['desc'] = ftsearch_strip_html($r['desc']);
 		ftsearch_set_entries(3, $id, $r['cache_id'], $r['desc'], $r['last_modified']);
 	}
@@ -254,7 +394,21 @@ function ftsearch_refresh_cache_desc($id)
 
 function ftsearch_refresh_all_pictures()
 {
-	$rs = sql('SELECT `pictures`.`id` FROM `pictures` LEFT JOIN `search_index_times` ON `pictures`.`id`=`search_index_times`.`object_id` AND 6=`search_index_times`.`object_type` WHERE ISNULL(`search_index_times`.`object_id`) UNION DISTINCT SELECT `pictures`.`id` FROM `pictures` INNER JOIN `search_index_times` ON `search_index_times`.`object_type`=6 AND `pictures`.`id`=`search_index_times`.`object_id` WHERE `pictures`.`last_modified`>`search_index_times`.`last_refresh`');
+	$rs = sql(
+        '
+        SELECT `pictures`.`id`
+        FROM `pictures`
+        LEFT JOIN `search_index_times`
+            ON `pictures`.`id`=`search_index_times`.`object_id`
+            AND 6=`search_index_times`.`object_type`
+        WHERE ISNULL(`search_index_times`.`object_id`)
+        UNION DISTINCT
+        SELECT `pictures`.`id`
+        FROM `pictures`
+        INNER JOIN `search_index_times`
+            ON `search_index_times`.`object_type`= 6
+            AND `pictures`.`id`=`search_index_times`.`object_id`
+        WHERE `pictures`.`last_modified`>`search_index_times`.`last_refresh`');
 	while ($r = sql_fetch_assoc($rs))
 		ftsearch_refresh_picture($r['id']);
 	sql_free_result($rs);
@@ -262,9 +416,31 @@ function ftsearch_refresh_all_pictures()
 
 function ftsearch_refresh_picture($id)
 {
-	$rs = sql("SELECT `caches`.`cache_id`, `pictures`.`title`, `pictures`.`last_modified` FROM `pictures` INNER JOIN `caches` ON `pictures`.`object_type`=2 AND `caches`.`cache_id`=`pictures`.`object_id` WHERE `pictures`.`id`='&1' UNION DISTINCT SELECT `cache_logs`.`cache_id` , `pictures`.`title`, `pictures`.`last_modified` FROM `pictures` INNER JOIN `cache_logs` ON `pictures`.`object_type`=1 AND `cache_logs`.`id`=`pictures`.`object_id` WHERE `pictures`.`id`='&1' LIMIT 1", $id);
-	if ($r = sql_fetch_assoc($rs))
-	{
+	$rs = sql(
+        "
+        SELECT
+            `caches`.`cache_id`,
+            `pictures`.`title`,
+            `pictures`.`last_modified`
+        FROM `pictures`
+        INNER JOIN `caches`
+            ON `pictures`.`object_type`=2
+            AND `caches`.`cache_id`=`pictures`.`object_id`
+        WHERE `pictures`.`id`='&1'
+        UNION DISTINCT
+        SELECT
+            `cache_logs`.`cache_id`,
+            `pictures`.`title`,
+            `pictures`.`last_modified`
+        FROM `pictures`
+        INNER JOIN `cache_logs`
+            ON `pictures`.`object_type`= 1
+            AND `cache_logs`.`id`=`pictures`.`object_id`
+        WHERE `pictures`.`id`='&1' 
+        LIMIT 1",
+        $id
+    );
+	if ($r = sql_fetch_assoc($rs)) {
 		ftsearch_set_entries(6, $id, $r['cache_id'], $r['title'], $r['last_modified']);
 	}
 	sql_free_result($rs);
@@ -272,17 +448,32 @@ function ftsearch_refresh_picture($id)
 
 function ftsearch_refresh_all_cache_logs()
 {
-	$rs = sql('SELECT `cache_logs`.`id` FROM `cache_logs` LEFT JOIN `search_index_times` ON `cache_logs`.`id`=`search_index_times`.`object_id` AND 1=`search_index_times`.`object_type` WHERE ISNULL(`search_index_times`.`object_id`) UNION DISTINCT SELECT `cache_logs`.`id` FROM `cache_logs` INNER JOIN `search_index_times` ON `search_index_times`.`object_type`=1 AND `cache_logs`.`id`=`search_index_times`.`object_id` WHERE `cache_logs`.`last_modified`>`search_index_times`.`last_refresh`');
-	while ($r = sql_fetch_assoc($rs))
-		ftsearch_refresh_cache_logs($r['id']);
+	$rs = sql(
+        '
+        SELECT `cache_logs`.`id`
+        FROM `cache_logs`
+        LEFT JOIN `search_index_times`
+            ON `cache_logs`.`id`=`search_index_times`.`object_id`
+            AND 1=`search_index_times`.`object_type`
+        WHERE ISNULL(`search_index_times`.`object_id`)
+        UNION DISTINCT
+        SELECT `cache_logs`.`id`
+        FROM `cache_logs`
+        INNER JOIN `search_index_times`
+            ON `search_index_times`.`object_type`= 1
+            AND `cache_logs`.`id`=`search_index_times`.`object_id`
+        WHERE `cache_logs`.`last_modified`>`search_index_times`.`last_refresh`'
+    );
+	while ($r = sql_fetch_assoc($rs)) {
+        ftsearch_refresh_cache_logs($r['id']);
+    }
 	sql_free_result($rs);
 }
 
 function ftsearch_refresh_cache_logs($id)
 {
 	$rs = sql("SELECT `cache_id`, `text`, `last_modified` FROM `cache_logs` WHERE `id`='&1'", $id);
-	if ($r = sql_fetch_assoc($rs))
-	{
+	if ($r = sql_fetch_assoc($rs)) {
 		$r['text'] = ftsearch_strip_html($r['text']);
 		ftsearch_set_entries(1, $id, $r['cache_id'], $r['text'], $r['last_modified']);
 	}
@@ -300,8 +491,7 @@ function ftsearch_set_entries($object_type, $object_id, $cache_id, &$text, $last
 	ftsearch_delete_entries($object_type, $object_id, $cache_id);
 
 	$ahash = ftsearch_hash($text);
-	foreach ($ahash AS $k => $h)
-	{
+	foreach ($ahash AS $k => $h) {
 		sql("INSERT INTO `search_index` (`object_type`, `cache_id`, `hash`, `count`) VALUES ('&1', '&2', '&3', '&4') ON DUPLICATE KEY UPDATE `count`=`count`+1", $object_type, $cache_id, $h, 1);
 	}
 	sql("INSERT INTO `search_index_times` (`object_id`, `object_type`, `last_refresh`) VALUES ('&1', '&2', '&3') ON DUPLICATE KEY UPDATE `last_refresh`='&3'", $object_id, $object_type, $last_modified);
--- htdocs/lib2/search/search.ov2.inc.php
+++ htdocs/lib2/search/search.ov2.inc.php
@@ -1,36 +1,36 @@
 <?php
-	/***************************************************************************
-		For license information see doc/license.txt
+/***************************************************************************
+ * For license information see doc/license.txt
+ *
+ * Unicode Reminder メモ
+ *
+ * OV2 search output
+ ****************************************************************************/
 
-		Unicode Reminder メモ
+require_once($opt['rootpath'] . 'lib2/charset.inc.php');
 
-		OV2 search output
-	****************************************************************************/
-
-	require_once($opt['rootpath'] . 'lib2/charset.inc.php');
-
-	$search_output_file_download = true;
-	$content_type_plain = 'application/ov2';
+$search_output_file_download = true;
+$content_type_plain = 'application/ov2';
 
 
 function search_output()
 {
-	global $sqldebug;
+    global $sqldebug;
 
-	/*
-		cacheid
-		name
-		latitude
-		longitude
-		type
-		size
-		difficulty
-		terrain
-		username
-		waypoint
-	*/
+    /*
+        cacheid
+        name
+        latitude
+        longitude
+        type
+        size
+        difficulty
+        terrain
+        username
+        waypoint
+    */
 
-	$sql = '
+    $sql = '
 		SELECT
 			&searchtmp.`cache_id` `cacheid`,
 			&searchtmp.`longitude`,
@@ -54,30 +54,29 @@ function search_output()
 			&searchtmp.`size`=`cache_size`.`id` AND
 			&searchtmp.`user_id`=`user`.`user_id`';
 
-	$rs = sql_slave($sql, $sqldebug);
+    $rs = sql_slave($sql, $sqldebug);
 
-	while ($r = sql_fetch_array($rs))
-	{
-		$lat = sprintf('%07d', $r['latitude'] * 100000);
-		$lon = sprintf('%07d', $r['longitude'] * 100000);
-		$name = convert_string($r['name']);
-		$username = convert_string($r['username']);
-		$type = convert_string($r['typedesc']);
-		$size = convert_string($r['sizedesc']);
-		$difficulty = sprintf('%01.1f', $r['difficulty'] / 2);
-		$terrain = sprintf('%01.1f', $r['terrain'] / 2);
-		$cacheid = convert_string($r['wp_oc']);
+    while ($r = sql_fetch_array($rs)) {
+        $lat = sprintf('%07d', $r['latitude'] * 100000);
+        $lon = sprintf('%07d', $r['longitude'] * 100000);
+        $name = convert_string($r['name']);
+        $username = convert_string($r['username']);
+        $type = convert_string($r['typedesc']);
+        $size = convert_string($r['sizedesc']);
+        $difficulty = sprintf('%01.1f', $r['difficulty'] / 2);
+        $terrain = sprintf('%01.1f', $r['terrain'] / 2);
+        $cacheid = convert_string($r['wp_oc']);
 
-		$line = "$name by $username, $type, $size, $cacheid";
-		$record = pack("CLllA*x", 2, 1 + 4 + 4 + 4 + strlen($line) + 1, (int)$lon, (int)$lat, $line);
+        $line = "$name by $username, $type, $size, $cacheid";
+        $record = pack("CLllA*x", 2, 1 + 4 + 4 + 4 + strlen($line) + 1, (int)$lon, (int)$lat, $line);
 
-		append_output($record);
-	}
-	mysql_free_result($rs);
+        append_output($record);
+    }
+    mysql_free_result($rs);
 }
 
 
-	function convert_string($str)
-	{
-		return utf8ToIso88591($str);
-	}
+function convert_string($str)
+{
+    return utf8ToIso88591($str);
+}

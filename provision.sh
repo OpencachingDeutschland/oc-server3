52309cdbc6f5e07e65279cb35b035adf0560d053
Thomas Eiling <teiling88@gmail.com>
2016-05-10 23:32:27 +0200

    adding composer install to provision.sh

--- provision.sh
+++ provision.sh
@@ -158,3 +158,12 @@ mysql -uroot -proot < opencaching_dump.sql
 echo "apply sql deltas"
 cd /var/www/html/ && php bin/dbupdate.php
 cd /var/www/html/ && php bin/dbsv-update.php
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/bin/composer
+chmod 777 /usr/bin/composer
+
+cd /var/www/html/htdocs && composer install --ignore-platform-reqs

7801d5410ad781f133f68a1ecefc1e0cd3a18599
Thomas Eiling <teiling88@gmail.com>
2016-05-12 22:12:10 +0200

    reformat local/maintenance directory

--- local/maintenance/init_user_languages.php
+++ local/maintenance/init_user_languages.php
@@ -1,38 +1,41 @@
 <?php
- /***************************************************************************
+/***************************************************************************
  *  For license information see doc/license.txt
  *
- *	This script guesses the primary language of all users who did not login
+ *    This script guesses the primary language of all users who did not login
  *  after release of OC 3.0.14.
  *
  *  Unicode Reminder メモ
  ***************************************************************************/
 
-	$opt['rootpath'] = __DIR__ . '/../../htdocs/';
-	require($opt['rootpath'] . 'lib2/cli.inc.php');
-	require($opt['rootpath'] . 'lib2/logic/user.class.php');
+$opt['rootpath'] = __DIR__ . '/../../htdocs/';
+require_once($opt['rootpath'] . 'lib2/cli.inc.php');
+require_once($opt['rootpath'] . 'lib2/logic/user.class.php');
 
-	$write = ($argc == 2 && $argv[1] = 'write');
-	if (!$write)
-		echo "use parameter 'write' to write changes to database\n";
-	$processed = 0;
-	$set = 0;
+$write = ($argc == 2 && $argv[1] = 'write');
+if (!$write) {
+    echo "use parameter 'write' to write changes to database\n";
+}
+$processed = 0;
+$set = 0;
 
-	$rs = sql("
-		SELECT `user_id` FROM `user` WHERE `language` IS NULL");
-	while ($r = sql_fetch_assoc($rs))
-	{
-		$user = new user($r['user_id']);
-		$lang = $user->guessLanguage();
-		if ($lang)
-		{
-			if ($write)
-				sql("UPDATE `user` SET `language`='&2', `language_guessed`=1 WHERE `user_id`='&1'",
-				    $r['user_id'], $lang);
-			++$set;
-		}
-		if (++$processed % 1000 == 0)
-			echo "$set of $processed " . ($write ? '' : 'would be ') . "set\n";
-	}
-	echo "$set of $processed " . ($write ? '' : 'would be ') . "set\n";
-	sql_free_result($rs);
+$rs = sql('SELECT `user_id` FROM `user` WHERE `language` IS NULL');
+while ($r = sql_fetch_assoc($rs)) {
+    $user = new user($r['user_id']);
+    $lang = $user->guessLanguage();
+    if ($lang) {
+        if ($write) {
+            sql(
+                "UPDATE `user` SET `language`='&2', `language_guessed`=1 WHERE `user_id`='&1'",
+                $r['user_id'],
+                $lang
+            );
+        }
+        ++ $set;
+    }
+    if (++ $processed % 1000 == 0) {
+        echo "$set of $processed " . ($write ? '' : 'would be ') . "set\n";
+    }
+}
+echo "$set of $processed " . ($write ? '' : 'would be ') . "set\n";
+sql_free_result($rs);
--- local/stat/coordstat.php
+++ local/stat/coordstat.php
@@ -2,31 +2,31 @@
 /***************************************************************************
  *  For license information see doc/license.txt
  *
-  *  Unicode Reminder メモ
-***************************************************************************/
+ *  Unicode Reminder メモ
+ ***************************************************************************/
 
 // statistical data for cache and log activity map
 
-chdir ("../htdocs");
+chdir("../htdocs");
 require('lib2/web.inc.php');
 
 error_reporting(error_reporting() & ~E_NOTICE);
 
 $grid = $_GET["grid"];
-if ($grid <= 0) $grid = 0.2;
+if ($grid <= 0) {
+    $grid = 0.2;
+}
 
 // caches created by year
 $rs = sql("SELECT latitude, longitude, date_created FROM caches");
-while ($cache = sql_fetch_assoc($rs))
-{
-  $lat = floor($cache["latitude"] / $grid);
-  $long = floor($cache["longitude"] / $grid);
-  $year = substr($cache["date_created"],0,4);
-  if ($year >= 2005 && $year <= date("Y") && ($lat != 0 || $long != 0))
-  {
-    $years[$year] = true;
-    $liste[$lat][$long]["caches"][$year]++;
-  }
+while ($cache = sql_fetch_assoc($rs)) {
+    $lat = floor($cache["latitude"] / $grid);
+    $long = floor($cache["longitude"] / $grid);
+    $year = substr($cache["date_created"], 0, 4);
+    if ($year >= 2005 && $year <= date("Y") && ($lat != 0 || $long != 0)) {
+        $years[$year] = true;
+        $liste[$lat][$long]["caches"][$year] ++;
+    }
 }
 mysql_free_result($rs);
 
@@ -36,60 +36,64 @@ get_logs("cache_logs");
 
 function get_logs($table)
 {
-	global $grid, $liste, $years;
-	
-  $rs = sql("SELECT latitude, longitude, date
-	  				 FROM $table
-		  			 INNER JOIN caches ON $table.cache_id=caches.cache_id");
-  while ($cache = sql_fetch_assoc($rs))
-  {
-    $lat = floor($cache["latitude"] / $grid);
-    $long = floor($cache["longitude"] / $grid);
-    $year = substr($cache["date"],0,4);
-    if ($year >= 2005 && $year <= date("Y") && ($lat != 0 || $long != 0))
-    {
-      $years[$year] = true;
-      $liste[$lat][$long]["logs"][$year]++;
+    global $grid, $liste, $years;
+
+    $rs = sql(
+        "SELECT latitude, longitude, date
+         FROM $table
+		 INNER JOIN caches ON $table.cache_id=caches.cache_id"
+    );
+    while ($cache = sql_fetch_assoc($rs)) {
+        $lat = floor($cache["latitude"] / $grid);
+        $long = floor($cache["longitude"] / $grid);
+        $year = substr($cache["date"], 0, 4);
+        if ($year >= 2005 && $year <= date("Y") && ($lat != 0 || $long != 0)) {
+            $years[$year] = true;
+            $liste[$lat][$long]["logs"][$year] ++;
+        }
     }
-  }
-  mysql_free_result($rs);
+    mysql_free_result($rs);
 }
 
 ksort($years);
 
 // active caches and logs
-$rs = sql("SELECT latitude, longitude,
-           (SELECT COUNT(*) FROM cache_logs WHERE cache_logs.cache_id=caches.cache_id) AS logs
-           FROM caches WHERE status=1");
-while ($cache = sql_fetch_assoc($rs))
-{
-  $lat = floor($cache["latitude"] / $grid);
-  $long = floor($cache["longitude"] / $grid);
-  $liste[$lat][$long]["caches"]["all"]++;
-  $liste[$lat][$long]["logs"]["all"] += $cache["logs"];
+$rs = sql(
+    'SELECT latitude, longitude,
+     (SELECT COUNT(*) FROM cache_logs WHERE cache_logs.cache_id=caches.cache_id) AS logs
+     FROM caches WHERE status=1'
+);
+while ($cache = sql_fetch_assoc($rs)) {
+    $lat = floor($cache["latitude"] / $grid);
+    $long = floor($cache["longitude"] / $grid);
+    $liste[$lat][$long]["caches"]["all"] ++;
+    $liste[$lat][$long]["logs"]["all"] += $cache["logs"];
 }
 mysql_free_result($rs);
 
-ksort ($liste);
+ksort($liste);
 $lats = array_keys($liste);
-foreach ($lats as $lat)
-  ksort($liste[$lat]);
-  
+foreach ($lats as $lat) {
+    ksort($liste[$lat]);
+}
+
 // create output CSV data
-header("Content-type: application/comma-separated-value");
+header('Content-type: application/comma-separated-value');
 header('Content-Disposition: attachment; filename="cachestat.csv"');
-echo "latitude,longitude,caches,logs";
-foreach ($years as $year => $dummy)
-  echo ",caches$year,logs$year";
+echo 'latitude,longitude,caches,logs';
+foreach ($years as $year => $dummy) {
+    echo ",caches$year,logs$year";
+}
 echo "\r\n";
 
-foreach ($liste as $lat => $liste2)
-  foreach ($liste2 as $long => $cachecounts)
-  {
-    echo ($lat*$grid + 0.5*$grid) . "," . ($long*$grid + 0.5*$grid) . "," .
-         $cachecounts["caches"]["all"] . "," .
-         $cachecounts["logs"]["all"];
-		foreach ($years as $year => $dummy)
-		  echo "," . $cachecounts["caches"][$year] . "," . $cachecounts["logs"][$year];
-		echo  "\r\n";
-	}
+foreach ($liste as $lat => $liste2) {
+    foreach ($liste2 as $long => $cachecounts) {
+        echo ($lat * $grid + 0.5 * $grid) . ',' . ($long * $grid + 0.5 * $grid) . ',' .
+            $cachecounts['caches']['all'] . ',' .
+            $cachecounts['logs']['all'];
+        foreach ($years as $year => $dummy) {
+            echo ',' . $cachecounts["caches"][$year] . ',' . $cachecounts["logs"][$year];
+        }
+        echo "\r\n";
+    }
+}

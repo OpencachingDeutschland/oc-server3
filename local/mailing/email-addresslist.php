f3a423b2f57b0d76865b435b2aedfde65d77ab24
Thomas Eiling <teiling88@gmail.com>
2016-05-12 22:10:45 +0200

    reformat local/mailing directory

--- local/mailing/email-addresslist.php
+++ local/mailing/email-addresslist.php
@@ -1,16 +1,22 @@
 <?php
-	//	For license information see doc/license.txt
+/***************************************************************************
+ *  For license information see doc/license.txt
+ *
+ *  Unicode Reminder メモ
+ ***************************************************************************/
 
-  $opt['rootpath'] = '../htdocs/';
-  header('Content-type: text/html; charset=utf-8');
-  require($opt['rootpath'] . 'lib2/web.inc.php');
+$opt['rootpath'] = __DIR__ . '/../../htdocs/';
+header('Content-type: text/html; charset=utf-8');
+require(__DIR__ . '/../../htdocs/lib2/web.inc.php');
 
-
-  $rs = sql("SELECT email FROM user
-						 WHERE NOT ISNULL(email) AND is_active_flag!=0 AND email_problems=0
-						 ORDER BY user_id DESC");
-  while ($r = sql_fetch_assoc($rs))
-  {
+$rs = sql(
+    'SELECT email FROM `user`
+     WHERE NOT ISNULL(email)
+     AND is_active_flag!=0
+     AND email_problems=0
+     ORDER BY user_id DESC'
+);
+while ($r = sql_fetch_assoc($rs)) {
     echo $r['email'] . "\n";
-  }
-  sql_free_result($rs);
+}
+sql_free_result($rs);

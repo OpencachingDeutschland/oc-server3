971e7b4ddcffc57739c969fe5e69d9243495a90c
Thomas Eiling <teiling88@gmail.com>
2016-05-12 23:15:31 +0200

    reformat local/tools directory

--- local/tools/find_unicode_boms.php
+++ local/tools/find_unicode_boms.php
@@ -5,19 +5,16 @@
  *  Unicode Reminder メモ
  *
  *  Searches for Unicode byte order marks in code and template files
-***************************************************************************/
+ ***************************************************************************/
 
-chdir ("../../htdocs");
-require('lib2/cli.inc.php');
+chdir("../../htdocs");
+require_once('lib2/cli.inc.php');
 
 
 scan('.', false);
 
-foreach (
-	array('api', 'lang', 'lib', 'lib2', 'okapi', 'src', 'templates2', 'util', 'util2', 'xml')
-	as $dir)
-{
-	scan($dir,true);
+foreach (['api', 'lang', 'lib', 'lib2', 'okapi', 'src', 'templates2', 'util', 'util2', 'xml'] as $dir) {
+    scan($dir, true);
 }
 
 exit;
@@ -25,26 +22,27 @@ exit;
 
 function scan($dir, $subdirs)
 {
-	$hDir = opendir($dir);
-	if ($hDir !== false)
-	{
-		while (($file = readdir($hDir)) !== false)
-		{
-			$path = $dir . '/' . $file;
-			if (is_dir($path) && substr($file,0,1) != '.' && $subdirs)
-				scan($path,$subdirs);
-			else if (is_file($path))
-				if ((substr($file, -4) == '.tpl') || (substr($file, -4) == '.php'))
-					testforbom($path);
-		}
-		closedir($hDir);
-	}
+    $hDir = opendir($dir);
+    if ($hDir !== false) {
+        while (($file = readdir($hDir)) !== false) {
+            $path = $dir . '/' . $file;
+            if (is_dir($path) && substr($file, 0, 1) !== '.' && $subdirs) {
+                scan($path, $subdirs);
+            } else {
+                if (is_file($path) && ((substr($file, - 4) === '.tpl') || (substr($file, - 4) === '.php'))) {
+                    testforbom($path);
+                }
+            }
+        }
+        closedir($hDir);
+    }
 }
 
 
 function testforbom($path)
 {
-	$filestart = file_get_contents($path, false, null, 0, 2);
-	if (ord($filestart) > 126)
-		printf("%02X-%02X found in %s\n", ord($filestart), ord(substr($filestart,1)), $path);
+    $filestart = file_get_contents($path, false, null, 0, 2);
+    if (ord($filestart) > 126) {
+        printf("%02X-%02X found in %s\n", ord($filestart), ord(substr($filestart, 1)), $path);
+    }
 }

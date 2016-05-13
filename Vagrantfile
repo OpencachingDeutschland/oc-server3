92810110afb995d9f4b83c99e141d185fef33381
mirsch <ms@splinelab.de>
2016-05-13 14:16:04 +0200

    fix shared folders on OS X/Linux, fix composer installation, add vagrant-hostmanager, fix vbguest plugin breaks VM

--- Vagrantfile
+++ Vagrantfile
@@ -1,34 +1,73 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+VM_IP		= "10.10.0.101"   # IP of the VM, must be unique in your network
+VM_MEMORY	= 512
+VM_CPUS		= 1
+VM_NAME		= "OpenCaching DEV VM"
+VM_HOST		= "local.opencaching.de"
+
+if Vagrant::Util::Platform.windows?
+  HOSTS_FILE = 'c:\WINDOWS\system32\drivers\etc\hosts'
+  SYNCED_FOLDER_TYPE = 'smb'
+else
+  HOSTS_FILE = '/etc/hosts'
+  SYNCED_FOLDER_TYPE = 'nfs'
+end
+
+
 # All Vagrant configuration is done below. The "2" in Vagrant.configure
 # configures the configuration version (we support older styles for
 # backwards compatibility). Please don't change it unless you know what
 # you're doing.
 Vagrant.configure(2) do |config|
-# The most common configuration options are documented and commented below.
-# For a complete reference, please see the online documentation at
-# https://docs.vagrantup.com.
-#
-config.vm.box = "bento/centos-7.1"
-config.vm.hostname = "local.opencaching.de"
-config.vm.network "forwarded_port", guest: 22, host: 2223 # ssh
-config.vm.network "forwarded_port", guest: 80, host: 80 # http
-config.vm.network "forwarded_port", guest: 443, host: 443 # https
-config.vm.network "forwarded_port", guest: 3306, host: 3306 # mysql
+	config.vm.box = "bento/centos-7.1"
+	config.vm.hostname = VM_HOST
+	config.vm.network "forwarded_port", guest: 80,		host: 80,  	auto_correct: true # http
+	config.vm.network "forwarded_port", guest: 443,		host: 443,  auto_correct: true # https
+	config.vm.network "forwarded_port", guest: 3306,	host: 3306,	auto_correct: true # mysql
 
-config.vm.synced_folder "bin/", "/var/www/html/bin", disabled: false
-config.vm.synced_folder "htdocs/", "/var/www/html/htdocs", disabled: false
-config.vm.synced_folder "doc/", "/var/www/html/doc", disabled: false
-config.vm.synced_folder "local/", "/var/www/html/local", disabled: false
+	config.vm.synced_folder "bin/", 	"/var/www/html/bin",	type: SYNCED_FOLDER_TYPE
+	config.vm.synced_folder "htdocs/",	"/var/www/html/htdocs",	type: SYNCED_FOLDER_TYPE
+	config.vm.synced_folder "doc/", 	"/var/www/html/doc",	type: SYNCED_FOLDER_TYPE
+	config.vm.synced_folder "local/", 	"/var/www/html/local",	type: SYNCED_FOLDER_TYPE
+	if SYNCED_FOLDER_TYPE == "nfs"
+		config.nfs.map_uid = Process.uid
+		config.nfs.map_gid = Process.gid
+	end
 
-config.vm.provider "virtualbox" do |v|
-    v.memory = 512
-    v.cpus = 1
-end
+	config.vm.provider :virtualbox do |v|
+		v.name = VM_NAME
+		v.customize([
+			"modifyvm", :id,
+			"--memory", VM_MEMORY,
+			"--cpus", VM_CPUS,
+		])
+	end
 
-config.vm.network :private_network, type: "dhcp"
+	config.vm.network :private_network, ip: VM_IP
 
-config.ssh.insert_key = false
-config.vm.provision "shell", path: "provision.sh"
+	if Vagrant.has_plugin? 'vagrant-hostmanager'
+		config.hostmanager.enabled = true
+		config.hostmanager.manage_host = true
+		config.hostmanager.ignore_private_ip = false
+		config.hostmanager.include_offline = true
+		config.hostmanager.aliases = [VM_HOST]
+	else
+		hosts_entry = VM_IP + " " + VM_HOST
+		if not File.open(HOSTS_FILE).each_line.any? { |line| line.chomp == hosts_entry }
+			puts "\e[31mvagrant-hostmanager plugin not installed.\nInstall it using 'vagrant plugin install vagrant-hostmanager'"
+			puts "Alternatively manually add the following entry to your hosts file in #{HOSTS_FILE}\n"
+			puts hosts_entry
+			puts "\033[0m"
+		end
+	end
+
+	# the image is not compatible with latest vbguest additions,
+	# if you have vbguest plugin installed and it automatically tries to update
+	# it will break the VM
+	config.vbguest.auto_update = false
+
+	config.ssh.insert_key = false
+	config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
-sudo -s
+
+# enter url for SQL dump. e.g. http://opencaching.de/dump.sql
+DUMP_URL=""
+
+function label {
+    echo -e "\n\033[0;34m=> ${1}\033[0m\n"
+}
+
+function errorLabel {
+    echo -e "\n\033[0;31m=> ${1}\033[0m\n"
+}
+
+if [[ -z "$DUMP_URL" ]]; then
+	errorLabel "No dump url given.\nPlease edit provision.sh and enter an url for the sql dump\nRun 'vagrant provision' if you are done.\n\n"
+	exit 1
+fi
+
+label "Install required components"
 yum -y install mariadb-server mariadb
 systemctl start mariadb.service
 systemctl enable mariadb.service
@@ -14,21 +31,8 @@ yum -y install php-gd php-odbc php-pear php-xml php-xmlrpc php-mbstring
 yum -y install php-snmp php-soap curl curl-devel php-mysql php-pdo php-pecl-zip
 yum -y install vim vim-common mutt mlocate man-pages zip mod_ssl
 
-cat /etc/php.ini | sed -e 's/upload_max_filesize = 2M/upload_max_filesize = 10M/' > /etc/php.ini.tmp
-mv /etc/php.ini.tmp /etc/php.ini
 
-cat /etc/sysconfig/selinux | sed -e 's/SELINUX=permissive/SELINUX=disabled/' > /etc/sysconfig/selinux.tmp
-mv /etc/sysconfig/selinux.tmp /etc/sysconfig/selinux
-
-mysqladmin -u root password root
-
-mysql -u root -proot -e "CREATE USER 'opencaching';"
-mysql -u root -proot -e "GRANT SELECT, INSERT, UPDATE, REFERENCES, DELETE, CREATE, DROP, ALTER, INDEX, CREATE TEMPORARY TABLES, LOCK TABLES, EVENT ON \`opencaching\`.* TO 'opencaching'@'%';"
-mysql -u root -proot -e "GRANT GRANT OPTION ON \`opencaching\`.* TO 'opencaching'@'%';"
-mysql -u root -proot -e "SET PASSWORD FOR 'opencaching'@'%' = PASSWORD('opencaching');"
-
-
-printf "\n" | pecl install imagick
+label "Adjust Apache and MariaDB configuration"
 # set max_allowed_packet
 cat <<EOF > /etc/my.cnf.d/server.cnf
 [server]
@@ -142,28 +146,63 @@ EOF
 systemctl restart mariadb
 systemctl restart httpd
 
-#import sql dump
+cat /etc/sysconfig/selinux | sed -e 's/SELINUX=permissive/SELINUX=disabled/' > /etc/sysconfig/selinux.tmp
+mv /etc/sysconfig/selinux.tmp /etc/sysconfig/selinux
+
+
+label "Adjust php.ini"
+cat /etc/php.ini | sed -e 's/upload_max_filesize = 2M/upload_max_filesize = 10M/' > /etc/php.ini.tmp
+mv /etc/php.ini.tmp /etc/php.ini
+
+
+label "Install imagick"
+printf "\n" | pecl install imagick
+
+
+label "Setup database user"
+mysqladmin -u root password root
+mysql -u root -proot -e "CREATE USER 'opencaching';"
+mysql -u root -proot -e "GRANT SELECT, INSERT, UPDATE, REFERENCES, DELETE, CREATE, DROP, ALTER, INDEX, CREATE TEMPORARY TABLES, LOCK TABLES, EVENT ON \`opencaching\`.* TO 'opencaching'@'%';"
+mysql -u root -proot -e "GRANT GRANT OPTION ON \`opencaching\`.* TO 'opencaching'@'%';"
+mysql -u root -proot -e "SET PASSWORD FOR 'opencaching'@'%' = PASSWORD('opencaching');"
+
+
+label "Configure OpenCaching"
 cd /var/www/html
 cp ./htdocs/config2/settings-sample-vagrant.inc.php ./htdocs/config2/settings.inc.php
 cp ./htdocs/lib/settings-sample-vagrant.inc.php ./htdocs/lib/settings.inc.php
 
-echo "download sql dump"
-wget -q INSERT-URL-HERE -O opencaching_dump.sql.gz
 
-gzip -d opencaching_dump.sql.gz
-echo "import sql dump"
-mysql -uroot -proot < opencaching_dump.sql
-
-# apply sql deltas
-echo "apply sql deltas"
-cd /var/www/html/ && php bin/dbupdate.php
-cd /var/www/html/ && php bin/dbsv-update.php
-
+label "Install Composer"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/bin/composer
-chmod 777 /usr/bin/composer
+chmod 0777 /usr/bin/composer
 
+
+label "Composer install"
 cd /var/www/html/htdocs && composer install --ignore-platform-reqs
+
+
+label "Install Database Dump from '$DUMP_URL'"
+label "Download SQL Dump"
+wget -q "$DUMP_URL" -O opencaching_dump.sql.gz
+gzip -d opencaching_dump.sql.gz
+
+if [ -f opencaching_dump.sql ]; then
+	label "Import SQL Dump"
+	mysql -uroot -proot < opencaching_dump.sql
+
+	label "Apply SQL deltas"
+	cd /var/www/html/ && php bin/dbupdate.php
+	cd /var/www/html/ && php bin/dbsv-update.php
+else
+	errorLabel "Could not download or unpack sql dump from '$DUMP_URL'\n\n"
+	exit 1;
+fi
+
+echo "export PS1='\[\033[1;33m\]OCdev:\[\033[m\w\$ '" >> .bashrc
+
+label "All done, have fun."

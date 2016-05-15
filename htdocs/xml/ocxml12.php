e05f593c26d1ed89480fb2bf463420f2996e45c4
Thomas Eiling <teiling88@gmail.com>
2016-05-15 22:56:55 +0200

    remove tabs  in htdocs/xml

--- htdocs/xml/ocxml12.php
+++ htdocs/xml/ocxml12.php
@@ -1,12 +1,11 @@
 <?php
 /***************************************************************************
-															 ./xml/ocxml12.php
-
-		For license information see doc/license.txt
-
-		Unicode Reminder メモ
-
-	***************************************************************************/
+ * ./xml/ocxml12.php
+ *
+ * For license information see doc/license.txt
+ *
+ * Unicode Reminder メモ
+ ***************************************************************************/
 
 $ocxmlversion = 12;
-include("ocxml11.php");
+include('ocxml11.php');

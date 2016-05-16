6fe453d1ad2632a2c7c1f4ededa7aa389a3b83d4
following <following@online.de>
2016-05-16 22:57:47 +0200

    resolved tabs

--- htdocs/resource2/tinymce/config/desc.js.php
+++ htdocs/resource2/tinymce/config/desc.js.php
@@ -13,38 +13,38 @@
   }
 ?>
 tinyMCE_GZ.init({
-	plugins : 'advhr,contextmenu,emotions,insertdatetime,paste,table',
-	themes : 'advanced',
-	languages : '<?php echo $lang; ?>',
-	disk_cache : true,
-	debug : false
+    plugins : 'advhr,contextmenu,emotions,insertdatetime,paste,table',
+    themes : 'advanced',
+    languages : '<?php echo $lang; ?>',
+    disk_cache : true,
+    debug : false
 });
 tinyMCE.init({
-	mode : "textareas",
-	theme : "advanced",
+    mode : "textareas",
+    theme : "advanced",
 
-	plugins : "advhr,contextmenu,emotions,insertdatetime,paste,table",
+    plugins : "advhr,contextmenu,emotions,insertdatetime,paste,table",
 
-	theme_advanced_buttons1 : "cut,copy,paste,pasteword,pastetext,removeformat,separator,undo,redo,separator,link,unlink,image,separator,fontselect,fontsizeselect",
-	theme_advanced_buttons2 : "bold,italic,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,insertdate,inserttime,separator,forecolor,backcolor,charmap,emotions",
-	theme_advanced_buttons3 : "visualaid,tablecontrols,separator,advhr",
+    theme_advanced_buttons1 : "cut,copy,paste,pasteword,pastetext,removeformat,separator,undo,redo,separator,link,unlink,image,separator,fontselect,fontsizeselect",
+    theme_advanced_buttons2 : "bold,italic,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,insertdate,inserttime,separator,forecolor,backcolor,charmap,emotions",
+    theme_advanced_buttons3 : "visualaid,tablecontrols,separator,advhr",
 
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_path_location : "bottom",
-	plugin_insertdate_dateFormat : "%Y-%m-%d",
-	plugin_insertdate_timeFormat : "%H:%M:%S",
-	file_browser_callback : "imageBrowser",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_path_location : "bottom",
+    plugin_insertdate_dateFormat : "%Y-%m-%d",
+    plugin_insertdate_timeFormat : "%H:%M:%S",
+    file_browser_callback : "imageBrowser",
 
-	theme_advanced_resize_horizontal : false,
-	theme_advanced_resizing : true,
-	editor_deselector : "mceNoEditor",
-	language : "<?php echo $lang; ?>",
-	preformatted : true,
-	remove_linebreaks : false,
-	oninit : "postEditorInit",
+    theme_advanced_resize_horizontal : false,
+    theme_advanced_resizing : true,
+    editor_deselector : "mceNoEditor",
+    language : "<?php echo $lang; ?>",
+    preformatted : true,
+    remove_linebreaks : false,
+    oninit : "postEditorInit",
 
-	content_css : "resource2/tinymce/config/content.css"
+    content_css : "resource2/tinymce/config/content.css"
 });
 
 var fileBrowserReturnURL = "";
--- htdocs/resource2/tinymce/config/list.js.php
+++ htdocs/resource2/tinymce/config/list.js.php
@@ -6,39 +6,39 @@
 ?>
 
 tinyMCE_GZ.init({
-	plugins : 'advhr,contextmenu,emotions,insertdatetime,paste,table',
-	themes : 'advanced',
-	languages : '<?php echo $lang; ?>',
-	disk_cache : true,
-	debug : false
+    plugins : 'advhr,contextmenu,emotions,insertdatetime,paste,table',
+    themes : 'advanced',
+    languages : '<?php echo $lang; ?>',
+    disk_cache : true,
+    debug : false
 });
 tinyMCE.init({
-	mode : "textareas",
-	theme : "advanced",
+    mode : "textareas",
+    theme : "advanced",
 
-	plugins : "advhr,contextmenu,emotions,insertdatetime,paste,table",
+    plugins : "advhr,contextmenu,emotions,insertdatetime,paste,table",
 
-	width: 580,
-	height: 220,
+    width: 580,
+    height: 220,
 
-	theme_advanced_buttons1 : "cut,copy,paste,pasteword,pastetext,removeformat,separator,undo,redo,separator,link,unlink,image,separator,fontselect,fontsizeselect",
-	theme_advanced_buttons2 : "bold,italic,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,insertdate,inserttime,separator,forecolor,backcolor,charmap,emotions,separator,visualaid,advhr",
-	theme_advanced_buttons3 : "",
+    theme_advanced_buttons1 : "cut,copy,paste,pasteword,pastetext,removeformat,separator,undo,redo,separator,link,unlink,image,separator,fontselect,fontsizeselect",
+    theme_advanced_buttons2 : "bold,italic,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,insertdate,inserttime,separator,forecolor,backcolor,charmap,emotions,separator,visualaid,advhr",
+    theme_advanced_buttons3 : "",
 
-	theme_advanced_toolbar_location : "top",
-	theme_advanced_toolbar_align : "left",
-	theme_advanced_path_location : "bottom",
-	plugin_insertdate_dateFormat : "%Y-%m-%d",
-	plugin_insertdate_timeFormat : "%H:%M:%S",
-	file_browser_callback : "",
+    theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+    theme_advanced_path_location : "bottom",
+    plugin_insertdate_dateFormat : "%Y-%m-%d",
+    plugin_insertdate_timeFormat : "%H:%M:%S",
+    file_browser_callback : "",
 
-	theme_advanced_resize_horizontal : false,
-	theme_advanced_resizing : true,
-	editor_deselector : "mceNoEditor",
-	language : "<?php echo $lang; ?>",
-	preformatted : true,
-	remove_linebreaks : false,
-	oninit : "postEditorInit",
+    theme_advanced_resize_horizontal : false,
+    theme_advanced_resizing : true,
+    editor_deselector : "mceNoEditor",
+    language : "<?php echo $lang; ?>",
+    preformatted : true,
+    remove_linebreaks : false,
+    oninit : "postEditorInit",
 
-	content_css : "resource2/tinymce/config/content.css"
+    content_css : "resource2/tinymce/config/content.css"
 });

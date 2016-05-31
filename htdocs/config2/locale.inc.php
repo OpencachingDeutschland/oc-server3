OpencachingDeutschland/oc-server3

commit 7a7e68a8af9a331da6002dca52fe3bff052c661a

require and include refactoring
- unified all require and include statements to the simplest form
- removed obsolete class file includes
- moved rowEditorClass.php from lib2/logic to lib2
- moved autoload.php inclusion from lib/common.php to lib/clicompatbase.inc.php
- some bugfixes
- some PSR-2 adjustments
- some comment formatting

htdocs/newlogs.php
@@ -5,7 +5,7 @@
  *  Unicode Reminder メモ
  ***************************************************************************/
 
-require_once('./lib2/web.inc.php');
+require 'lib2/web.inc.php';
 
 $add_where = '';
 $newLogsPerCountry = $opt['logic']['new_logs_per_country'];

commit 19872b483747eeeba673f03f9a2f2abfa778d5aa

some bugfixes - fixed require error introduced with 7a7e68a8a - fixed language definition error introduced with 9ffdddcf

htdocs/config2/locale.inc.php
@@ -229,6 +229,7 @@ $opt['locale']['SV']['format']['phpdate'] = 'Y-m-d';
 $opt['locale']['SV']['format']['phpdatetime'] = 'Y-m-d H:i';
 $opt['locale']['SV']['format']['colonspace'] = '';
 $opt['locale']['SV']['country'] = 'SE';
+$opt['locale']['SV']['primary_lang_of'] = ['SV'];
 $opt['locale']['SV']['page']['subtitle1'] = 'Geocaching med Opencaching';
 $opt['locale']['SV']['page']['subtitle2'] = '';
 $opt['locale']['SV']['mostly_translated'] = false;
@@ -246,6 +247,7 @@ $opt['locale']['NO']['format']['phpdate'] = 'd.m.Y';
 $opt['locale']['NO']['format']['phpdatetime'] = 'd.m.Y H:i';
 $opt['locale']['NO']['format']['colonspace'] = '';
 $opt['locale']['NO']['country'] = 'NO';
+$opt['locale']['NO']['primary_lang_of'] = ['NO'];
 $opt['locale']['NO']['page']['subtitle1'] = 'Geocaching med Opencaching';
 $opt['locale']['NO']['page']['subtitle2'] = '';
 $opt['locale']['NO']['mostly_translated'] = false;
@@ -263,6 +265,7 @@ $opt['locale']['PL']['format']['phpdate'] = 'd-m-Y';
 $opt['locale']['PL']['format']['phpdatetime'] = 'd-m-Y H:i';
 $opt['locale']['PL']['format']['colonspace'] = '';
 $opt['locale']['PL']['country'] = 'PL';
+$opt['locale']['PL']['primary_lang_of'] = ['PL'];
 $opt['locale']['PL']['mostly_translated'] = false;
 $opt['locale']['PL']['what3words'] = false;
 
@@ -278,6 +281,7 @@ $opt['locale']['RU']['format']['phpdate'] = 'd-m-Y';
 $opt['locale']['RU']['format']['phpdatetime'] = 'd-m-Y H:i';
 $opt['locale']['RU']['format']['colonspace'] = '';
 $opt['locale']['RU']['country'] = 'RU';
+$opt['locale']['RU']['primary_lang_of'] = ['RU'];
 $opt['locale']['RU']['mostly_translated'] = false;
 $opt['locale']['RU']['what3words'] = true;   // "beta"
 
@@ -293,6 +297,7 @@ $opt['locale']['DA']['format']['phpdate'] = 'd-m-Y';
 $opt['locale']['DA']['format']['phpdatetime'] = 'd-m-Y H:i';
 $opt['locale']['DA']['format']['colonspace'] = '';
 $opt['locale']['DA']['country'] = 'DK';
+$opt['locale']['RU']['primary_lang_of'] = ['DK', 'GL'];
 $opt['locale']['DA']['page']['subtitle1'] = 'Geocaching med Opencaching';
 $opt['locale']['DA']['page']['subtitle2'] = '';
 $opt['locale']['DA']['mostly_translated'] = false;
@@ -310,6 +315,7 @@ $opt['locale']['PT']['format']['phpdate'] = 'd-m-Y';
 $opt['locale']['PT']['format']['phpdatetime'] = 'd-m-Y H:i';
 $opt['locale']['PT']['format']['colonspace'] = '';
 $opt['locale']['PT']['country'] = 'PT';
+$opt['locale']['PT']['primary_lang_of'] = ['PT', 'AO', 'BR', 'CV', 'GW', 'MZ', 'ST'];
 $opt['locale']['PT']['page']['subtitle1'] = 'Geocaching com Opencaching';
 $opt['locale']['PT']['page']['subtitle2'] = '';
 $opt['locale']['PT']['mostly_translated'] = false;
@@ -327,6 +333,7 @@ $opt['locale']['JA']['format']['phpdate'] = 'd-m-Y';
 $opt['locale']['JA']['format']['phpdatetime'] = 'd-m-Y H:i';
 $opt['locale']['JA']['format']['colonspace'] = '';
 $opt['locale']['JA']['country'] = 'JP';
+$opt['locale']['RU']['primary_lang_of'] = ['JP'];
 $opt['locale']['JA']['page']['subtitle1'] = 'Opencachingとジオキャッシング';
 $opt['locale']['JA']['page']['subtitle2'] = '';
 $opt['locale']['JA']['mostly_translated'] = false;

htdocs/newlogs.php
@@ -5,7 +5,7 @@
  *  Unicode Reminder メモ
  ***************************************************************************/
 
-require __DIR__ . '/lib2/web.inc.php';
+require_once __DIR__ . '/lib2/web.inc.php';
 
 $add_where = '';
 $newLogsPerCountry = $opt['logic']['new_logs_per_country'];
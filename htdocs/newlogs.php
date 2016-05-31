OpencachingDeutschland/oc-server3

commit 7a7e68a8af9a331da6002dca52fe3bff052c661a

require and include refactoring
- unified all require and include statements to the simplest form
- removed obsolete class file includes
- moved rowEditorClass.php from lib2/logic to lib2
- moved autoload.php inclusion from lib/common.php to lib/clicompatbase.inc.php
- some bugfixes
- some PSR-2 adjustments
- some comment formatting

htdocs/newlogs.php
@@ -5,7 +5,7 @@
  *  Unicode Reminder メモ
  ***************************************************************************/
 
-require_once('./lib2/web.inc.php');
+require 'lib2/web.inc.php';
 
 $add_where = '';
 $newLogsPerCountry = $opt['logic']['new_logs_per_country'];

commit 7132f55d1f09fe160e9f8caca2a4358a87c2e65f

fixed require error introduced with 7a7e68a8a

htdocs/newlogs.php
@@ -5,7 +5,7 @@
  *  Unicode Reminder メモ
  ***************************************************************************/
 
-require __DIR__ . '/lib2/web.inc.php';
+require_once __DIR__ . '/lib2/web.inc.php';
 
 $add_where = '';
 $newLogsPerCountry = $opt['logic']['new_logs_per_country'];
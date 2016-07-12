982382a70b0e815df7b6761bde5bba51b780d54f
Thomas Eiling <teiling88@gmail.com>
2016-07-12 06:29:22 +0200

    reformat phpunit test

--- tests/Modules/Lib2/Logic/CoordinateTest.php
+++ tests/Modules/Lib2/Logic/CoordinateTest.php
@@ -36,18 +36,18 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function utmProvider()
     {
-        return array(
-            array(51.52775, -120.89720, '10', 'U', 'N 5710611', 'E 645865'),
-            array(-8.81687, 13.24057, '33', 'L', 'N 9024939', 'E 306489'),
-            array(52.67578, 6.77300, '32', 'U', 'N 5838532', 'E 349438'),
-            array(60.63367, 4.81313, '32', 'V', 'N 6729280', 'E 271052'),
-            array(58.682079, 5.793595, '32', 'V', 'N 6509097', 'E 314134'),
-        );
+        return [
+            [51.52775, -120.89720, '10', 'U', 'N 5710611', 'E 645865'],
+            [-8.81687, 13.24057, '33', 'L', 'N 9024939', 'E 306489'],
+            [52.67578, 6.77300, '32', 'U', 'N 5838532', 'E 349438'],
+            [60.63367, 4.81313, '32', 'V', 'N 6729280', 'E 271052'],
+            [58.682079, 5.793595, '32', 'V', 'N 6509097', 'E 314134'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getUTM()
+     * @covers       \coordinate::getUTM()
      * @dataProvider utmProvider
      *
      * @return void
@@ -65,17 +65,17 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function gkProvider()
     {
-        return array(
-            array(51.52775, -120.89720, 'R -39562771 H 5710022'),
-            array(-8.81687, 13.24057, 'R 4636588 H -975608'),
-            array(52.67578, 6.77300, 'R 2552325 H 5838386'),
-            array(60.63367, 4.81313, 'R 2435086 H 6724824'),
-        );
+        return [
+            [51.52775, -120.89720, 'R -39562771 H 5710022'],
+            [-8.81687, 13.24057, 'R 4636588 H -975608'],
+            [52.67578, 6.77300, 'R 2552325 H 5838386'],
+            [60.63367, 4.81313, 'R 2435086 H 6724824'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getGK()
+     * @covers       \coordinate::getGK()
      * @dataProvider gkProvider
      *
      * @return void
@@ -91,17 +91,17 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function swissGridProvider()
     {
-        return array(
-            array(51.52775, -120.89720, '-3944504 / 8019927'),
-            array(-8.81687, 13.24057, '1499586 / -6904936'),
-            array(52.67578, 6.77300, '554738 / 837985'),
-            array(60.63367, 4.81313, '451121 / 1739767'),
-        );
+        return [
+            [51.52775, -120.89720, '-3944504 / 8019927'],
+            [-8.81687, 13.24057, '1499586 / -6904936'],
+            [52.67578, 6.77300, '554738 / 837985'],
+            [60.63367, 4.81313, '451121 / 1739767'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getSwissGrid()
+     * @covers       \coordinate::getSwissGrid()
      * @dataProvider swissGridProvider
      *
      * @return void
@@ -116,15 +116,15 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function dutchGridProvider()
     {
-        return array(
-            array(52.67578, 6.77300, 'X 248723 Y 521824'),
-            array(60.63367, 4.81313, 'X 123409 Y 1408833'),
-        );
+        return [
+            [52.67578, 6.77300, 'X 248723 Y 521824'],
+            [60.63367, 4.81313, 'X 123409 Y 1408833'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getRD()
+     * @covers       \coordinate::getRD()
      * @dataProvider dutchGridProvider
      *
      * @return void
@@ -140,17 +140,17 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function qthProvider()
     {
-        return array(
-            array(51.52775, -120.89720, 'CO91NM'),
-            array(-8.81687, 13.24057, 'JI61OE'),
-            array(52.67578, 6.77300, 'JO32JQ'),
-            array(60.63367, 4.81313, 'JP20JP'),
-        );
+        return [
+            [51.52775, -120.89720, 'CO91NM'],
+            [-8.81687, 13.24057, 'JI61OE'],
+            [52.67578, 6.77300, 'JO32JQ'],
+            [60.63367, 4.81313, 'JP20JP'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getQTH()
+     * @covers       \coordinate::getQTH()
      * @dataProvider qthProvider
      *
      * @return void
@@ -165,44 +165,67 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
 
     public function what3WordsProvider()
     {
-        return array(
-            array(52.473570, 13.371317, 'DE', 'gewinn.kopf.digitalen'),
-            array(60.168947, 24.958826, 'DE', 'kurzem.knie.ringen'),
-            array(45.999639, -1.213892, 'DE', 'bewohnbar.modernes.empfundenen'),
-            array(52.473570, 13.371317, 'EN', 'steer.removed.smashes'),
-            array(60.168947, 24.958826, 'EN', 'dished.mailing.starred'),
-            array(45.999639, -1.213892, 'EN', 'declaim.alright.loaning'),
-        );
+        return [
+            [52.473570, 13.371317, 'DE', 'gewinn.kopf.digitalen'],
+            [60.168947, 24.958826, 'DE', 'kurzem.knie.ringen'],
+            [45.999639, -1.213892, 'DE', 'bewohnbar.modernes.empfundenen'],
+            [52.473570, 13.371317, 'EN', 'steer.removed.smashes'],
+            [60.168947, 24.958826, 'EN', 'dished.mailing.starred'],
+            [45.999639, -1.213892, 'EN', 'declaim.alright.loaning'],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getW3W
+     * @covers       \coordinate::getW3W
      * @dataProvider what3WordsProvider
      *
-     * @return void
+     * @param $lat
+     * @param $lon
+     * @param $language
+     * @param $expectedW3W
      */
-    public function need_api_key_testWhat3Words($lat, $lon, $language, $expectedW3W)
+    public function testWhat3Words($lat, $lon, $language, $expectedW3W)
     {
         $coord = new \coordinate($lat, $lon);
-        $w3w   = $coord->getW3W($language);
+        $w3w = $coord->getW3W($language);
 
-        $this->assertEquals($expectedW3W, $w3w);
+        self::assertEquals($expectedW3W, $w3w);
     }
 
     public function formatProvider()
     {
-        return array(
-            array(51.52775, -120.89720, array('lat'=>"N 51° 31.665'", 'lon'=>"W 120° 53.832'"), array('lat'=>"N 51° 31' 39''", 'lon'=>"W 120° 53' 49''")),
-            array(-8.81687, 13.24057, array('lat'=>"S 08° 49.012'", 'lon'=>"E 013° 14.434'"), array('lat'=>"S 08° 49' 00''", 'lon'=>"E 013° 14' 26''")),
-            array(52.67578, 6.77300, array('lat'=>"N 52° 40.547'", 'lon'=>"E 006° 46.380'"), array('lat'=>"N 52° 40' 32''", 'lon'=>"E 006° 46' 22''")),
-            array(60.63367, 4.81313, array('lat'=>"N 60° 38.020'", 'lon'=>"E 004° 48.788'"), array('lat'=>"N 60° 38' 01''", 'lon'=>"E 004° 48' 47''")),
-        );
+        return [
+            [
+                51.52775,
+                -120.89720,
+                ['lat' => "N 51° 31.665'", 'lon' => "W 120° 53.832'"],
+                ['lat' => "N 51° 31' 39''", 'lon' => "W 120° 53' 49''"]
+            ],
+            [
+                -8.81687,
+                13.24057,
+                ['lat' => "S 08° 49.012'", 'lon' => "E 013° 14.434'"],
+                ['lat' => "S 08° 49' 00''", 'lon' => "E 013° 14' 26''"]
+            ],
+            [
+                52.67578,
+                6.77300,
+                ['lat' => "N 52° 40.547'", 'lon' => "E 006° 46.380'"],
+                ['lat' => "N 52° 40' 32''", 'lon' => "E 006° 46' 22''"]
+            ],
+            [
+                60.63367,
+                4.81313,
+                ['lat' => "N 60° 38.020'", 'lon' => "E 004° 48.788'"],
+                ['lat' => "N 60° 38' 01''", 'lon' => "E 004° 48' 47''"]
+            ],
+        ];
     }
 
     /**
      * @group unit-tests
-     * @covers \coordinate::getDecimalMinutes
+     * @covers       \coordinate::getDecimalMinutes
      * @dataProvider formatProvider
      *
      * @return void
@@ -211,7 +234,7 @@ class CoordinateTest extends \PHPUnit_Framework_TestCase
     {
         $coord = new \coordinate($lat, $lon);
 
-        $this->assertEquals($expectedMin, $coord->getDecimalMinutes());
-        $this->assertEquals($expectedMinSec, $coord->getDecimalMinutesSeconds());
+        self::assertEquals($expectedMin, $coord->getDecimalMinutes());
+        self::assertEquals($expectedMinSec, $coord->getDecimalMinutesSeconds());
     }
 }

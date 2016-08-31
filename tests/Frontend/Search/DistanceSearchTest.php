b46b1f4e96c233c6f8256774adee83b509461d56
Thomas Eiling <teiling88@gmail.com>
2016-08-31 17:19:59 +0200

    remove duplicate test method

--- tests/Frontend/Search/DistanceSearchTest.php
+++ tests/Frontend/Search/DistanceSearchTest.php
@@ -33,24 +33,4 @@ class DistanceSearchTest extends AbstractFrontendTest
         $searchTitle = $page->find('css', 'p.content-title-noshade-size15');
         self::assertContains('9 Caches gefunden', $searchTitle->getText());
     }
-
-    /**
-     * @group frontend
-     * @group frontend-search
-     *
-     * @throws ElementNotFoundException
-     *
-     * @return void
-     */
-    public function testInvalidOcWayPointSearch()
-    {
-        // check if issue with Undefined variable appears again ;-)
-        $this->login();
-        $this->session->visit($this->baseUrl . '/search.php');
-        $page = $this->session->getPage();
-        $page->fillField('userinput', 'asdf');
-        $page->pressButton('Go');
-        $pageTitle = $page->find('css', '.content2-pagetitle');
-        self::assertContains('Beim Aufruf der Seite ist ein Fehler aufgetreten.', $pageTitle->getText());
-    }
 }

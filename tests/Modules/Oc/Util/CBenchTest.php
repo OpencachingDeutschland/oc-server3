e737ba2b546c030d3ec00d8810bff4393b1ba254
Thomas Eiling <teiling88@gmail.com>
2016-10-29 10:32:55 +0200

    adding unit tests for CBench class

---
+++ tests/Modules/Oc/Util/CBenchTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace OcTest\Modules\Oc\Util;
+
+use Oc\Util\CBench;
+use OcTest\Modules\AbstractModuleTest;
+
+class CBenchTest extends AbstractModuleTest
+{
+    /**
+     * @var CBench
+     */
+    private $cBench;
+
+    public function setUp()
+    {
+        $this->cBench = new CBench();
+    }
+
+    public function testStartMethod()
+    {
+        self::assertEquals(0, $this->cBench->start);
+
+        $this->cBench->start();
+
+        self::assertInternalType('float', $this->cBench->start);
+    }
+
+    public function testStopMethod()
+    {
+        self::assertEquals(0, $this->cBench->stop);
+
+        $this->cBench->start();
+        $this->cBench->stop();
+
+        self::assertInternalType('float', $this->cBench->stop);
+        self::assertGreaterThan(0, $this->cBench->diff());
+        self::assertGreaterThan(0, $this->cBench->runTime());
+    }
+}

9db0c5d1fd0d5b2369c762394f4cb0d7877b8996
Thoams Eiling <teiling88@gmail.com>
2017-01-20 08:03:33 +0100

    misc - adding FRONTEND_URL placeholder

--- dev-ops/travis-ci/actions/unit.sh
+++ dev-ops/travis-ci/actions/unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-URL=http://127.0.0.1 phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --debug --verbose --coverage-html=build/artifacts/html-coverage
+URL=__FRONTEND_URL__ phpunit --stop-on-failure --stop-on-error --debug --verbose # --coverage-html=build/artifacts/html-coverage

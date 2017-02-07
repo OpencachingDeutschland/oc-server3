d330816d365d8eb759f39c73b3d4756377b66af7
Thoams Eiling <teiling88@gmail.com>
2017-02-07 20:35:44 +0100

    misc - add update.sh

---
+++ dev-ops/test.opencaching.de/actions/update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+INCLUDE: ./../../local.team-opencaching.de/actions/composer.sh
+INCLUDE: ./../../local.team-opencaching.de/actions/db-update.sh
+INCLUDE: ./../../local.team-opencaching.de/actions/import-sql-static.sh
+INCLUDE: ./../../local.team-opencaching.de/actions/sf-migrations.sh
+INCLUDE: ./../../local.team-opencaching.de/actions/okapi-update.sh
+INCLUDE: ./../../local.team-opencaching.de/actions/import-translations.sh

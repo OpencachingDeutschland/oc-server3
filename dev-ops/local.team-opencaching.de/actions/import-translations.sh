5d143b56521bf0ebb2805e59c1e085efb6e8affa
Thoams Eiling <teiling88@gmail.com>
2017-02-12 14:54:35 +0100

    misc - improve import translations command

--- dev-ops/local.team-opencaching.de/actions/import-translations.sh
+++ dev-ops/local.team-opencaching.de/actions/import-translations.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-curl -o translation.zip "http://www.opencaching.de/translations.zip"
-unzip -o htdocs/translation.zip
+cd htdocs && curl -o translation.zip "http://www.opencaching.de/translations.zip"
+cd htdocs && unzip -o translation.zip
+cd htdocs && rm translation.zip
 
 ./htdocs/bin/console translation:update de --force
 ./htdocs/bin/console translation:update en --force

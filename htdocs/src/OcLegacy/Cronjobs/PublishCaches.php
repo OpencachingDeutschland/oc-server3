a208a0b961c60e990b9df25a6293578691a31be8
Thoams Eiling <teiling88@gmail.com>
2017-03-08 22:15:37 +0100

    misc - fix PublishCaches Cronjob

--- htdocs/src/OcLegacy/Cronjobs/PublishCaches.php
+++ htdocs/src/OcLegacy/Cronjobs/PublishCaches.php
@@ -19,14 +19,16 @@ class PublishCaches
             AND NOT ISNULL(`date_activate`)
             AND `date_activate`<=NOW()'
         );
+
         while ($rPublish = sql_fetch_array($rsPublish)) {
             $userId = $rPublish['user_id'];
             $cacheId = $rPublish['cache_id'];
 
             // update cache status to active
             sql("SET @STATUS_CHANGE_USER_ID='&1'", $userId);
-            sql("UPDATE `caches` SET `status`=1, `date_activate`=NULL WHERE `cache_id`='&1'", $cacheId);
+            sql("UPDATE `caches` SET `status`=1, `date_activate` = NULL WHERE `cache_id`='&1'", $cacheId);
         }
+
         sql_free_result($rsPublish);
     }
 }

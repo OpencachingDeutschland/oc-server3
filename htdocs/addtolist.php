144ad217987837798736ff81c552d71b83277022
Thoams Eiling <teiling88@gmail.com>
2017-04-18 23:48:00 +0200

    improve coding style in addtolist.php

--- htdocs/addtolist.php
+++ htdocs/addtolist.php
@@ -9,58 +9,58 @@ $tpl->name = 'addtolist';
 $tpl->menuitem = MNU_CACHES_ADDTOLIST;
 
 $login->verify();
-if ($login->userid == 0) {
+if ($login->userid  == 0) {
     $tpl->redirect_login();
 }
 
-$cacheid = isset($_REQUEST['cacheid']) ? $_REQUEST['cacheid'] + 0 : 0;
-if (!$cacheid) {
+$cacheId = isset($_REQUEST['cacheid']) ? $_REQUEST['cacheid'] + 0 : 0;
+if (!$cacheId) {
     $tpl->redirect('index.php');
 }
-$tpl->assign('cacheid', $cacheid);
+$tpl->assign('cacheid', $cacheId);
 
 if (isset($_REQUEST['cancel'])) {
-    $tpl->redirect('viewcache.php?cacheid=' . $cacheid);
+    $tpl->redirect('viewcache.php?cacheid=' . $cacheId);
 }
 
-$newlist_name = isset($_REQUEST['newlist_name']) ? trim($_REQUEST['newlist_name']) : false;
-if ($newlist_name == $translate->t('New cache list', '', __FILE__, __LINE__)) {
-    $newlist_name = false;
+$newListName = isset($_REQUEST['newlist_name']) ? trim($_REQUEST['newlist_name']) : false;
+if ($newListName === $translate->t('New cache list', '', __FILE__, __LINE__)) {
+    $newListName = false;
 }
-$newlist_public = isset($_REQUEST['newlist_public']);
-$newlist_watch = isset($_REQUEST['newlist_watch']);
+$newListPublic = isset($_REQUEST['newlist_public']);
+$newListWatch = isset($_REQUEST['newlist_watch']);
 $default_list = isset($_REQUEST['listid']) ? (int)$_REQUEST['listid'] : cachelist::getMyLastAddedToListId();
 
-if (isset($_REQUEST['save']) && isset($_REQUEST['listid'])) {
-    $listid = $_REQUEST['listid'] + 0;
-    if ($listid == 0) {
-        $cachelist = new cachelist(ID_NEW, $login->userid);
-        $name_error = $cachelist->setNameAndVisibility($newlist_name, $newlist_public ? 2 : 0);
+if (isset($_REQUEST['save'], $_REQUEST['listid'])) {
+    $listId = (int) $_REQUEST['listid'];
+    if ($listId === 0) {
+        $cacheList = new cachelist(ID_NEW, $login->userid);
+        $name_error = $cacheList->setNameAndVisibility($newListName, $newListPublic ? 2 : 0);
         if ($name_error) {
             $tpl->assign('name_error', $name_error);
         } else {
-            $cachelist->setNode($opt['logic']['node']['id']);
-            if ($cachelist->save()) {
-                $cachelist->addCacheByID($cacheid);
-                if ($newlist_watch) {
-                    $cachelist->watch(true);
+            $cacheList->setNode($opt['logic']['node']['id']);
+            if ($cacheList->save()) {
+                $cacheList->addCacheByID($cacheId);
+                if ($newListWatch) {
+                    $cacheList->watch(true);
                 }
             }
-            $tpl->redirect('viewcache.php?cacheid=' . $cacheid);
+            $tpl->redirect('viewcache.php?cacheid=' . $cacheId);
         }
     } else {
-        $cachelist = new cachelist($listid);
-        if ($cachelist->exist()) {
-            $cachelist->addCacheByID($cacheid);
+        $cacheList = new cachelist($listId);
+        if ($cacheList->exist()) {
+            $cacheList->addCacheByID($cacheId);
         }
-        $tpl->redirect('viewcache.php?cacheid=' . $cacheid);
+        $tpl->redirect('viewcache.php?cacheid=' . $cacheId);
     }
 }
 
-$tpl->assign('cachename', sql_value("SELECT `name` FROM `caches` WHERE `cache_id`='&1'", '', $cacheid));
+$tpl->assign('cachename', sql_value("SELECT `name` FROM `caches` WHERE `cache_id`='&1'", '', $cacheId));
 $tpl->assign('cachelists', cachelist::getMyLists());
 $tpl->assign('default_list', $default_list);
-$tpl->assign('newlist_name', $newlist_name);
-$tpl->assign('newlist_public', $newlist_public);
-$tpl->assign('newlist_watch', $newlist_watch);
+$tpl->assign('newlist_name', $newListName);
+$tpl->assign('newlist_public', $newListPublic);
+$tpl->assign('newlist_watch', $newListWatch);
 $tpl->display();

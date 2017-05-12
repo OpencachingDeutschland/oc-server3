7c6415bcc8898b67db3d0ee1ed927bb06cb90d85
Thoams Eiling <teiling88@gmail.com>
2017-05-12 11:58:01 +0200

    adding test for smarty_modifier_escapejs

---
+++ tests/Modules/Oc/SmartyPlugins/ModifierEscapeJsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OcTest\Modules\Oc\SmartyPlugins;
+
+use OcTest\Modules\AbstractModuleTest;
+
+require_once __DIR__ . '/../../../../htdocs/src/Oc/SmartyPlugins/modifier.escapejs.php';
+
+class ModifierEscapeJsTest extends AbstractModuleTest
+{
+    public function testEscapeJs()
+    {
+        self::assertEquals(
+            '&quot;',
+            \smarty_modifier_escapejs('"')
+        );
+
+        self::assertEquals(
+            '\\\\',
+            \smarty_modifier_escapejs('\\')
+        );
+
+        self::assertEquals(
+            '\\\'',
+            \smarty_modifier_escapejs('\'')
+        );
+    }
+}

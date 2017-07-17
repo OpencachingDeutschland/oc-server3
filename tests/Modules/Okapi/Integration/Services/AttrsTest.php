700bd1339fb93f98c10f983095292a9ab7ec8edb
Thoams Eiling <teiling88@gmail.com>
2017-07-17 23:30:35 +0200

    misc - adding unit tests for okapi/services/attrs

---
+++ tests/Modules/Okapi/Integration/Services/AttrsTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace OcTest\Modules\Okapi\Integration\Services\Apisrv;
+
+use OcTest\Modules\AbstractModuleTest;
+use OcTest\Modules\Okapi\OkapiCredentialsTrait;
+
+class AttrsTest extends AbstractModuleTest
+{
+    use OkapiCredentialsTrait;
+
+    public function testAttrsAttributeIndexMethodNeedsConsumerKey()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get('services/attrs/attribute_index');
+
+        self::assertArrayHasKey('error', $response);
+        self::assertContains('(Level 1 Authentication)', $response['error']['developer_message']);
+    }
+
+
+    public function testAttrsAttributeIndexMethodReturnsAttributes()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get('services/attrs/attribute_index', ['consumer_key' => $this->getConsumerKey()]);
+
+        self::assertCount(77, $response);
+    }
+
+    public function testAttrsAttributeMethodNeedsConsumerKey()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get('services/attrs/attribute');
+
+        self::assertArrayHasKey('error', $response);
+        self::assertContains('(Level 1 Authentication)', $response['error']['developer_message']);
+    }
+
+    public function testAttrsAttributeMethodReturnsAttribute()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get(
+            'services/attrs/attribute',
+            ['consumer_key' => $this->getConsumerKey(), 'acode' => 'A1']
+        );
+
+        self::assertArrayHasKey('name', $response);
+        self::assertEquals('Listed at Opencaching only', $response['name']);
+    }
+
+
+    public function testAttrsAttributesMethodNeedsConsumerKey()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get('services/attrs/attributes');
+
+        self::assertArrayHasKey('error', $response);
+        self::assertContains('(Level 1 Authentication)', $response['error']['developer_message']);
+    }
+
+    public function testAttrsAttributesMethodReturnsAttribute()
+    {
+        $client = $this->createOkapiClient();
+
+        $response = $client->get(
+            'services/attrs/attributes',
+            ['consumer_key' => $this->getConsumerKey(), 'acodes' => 'A1|A2']
+        );
+
+        self::assertCount(2, $response);
+
+        self::assertArrayHasKey('A1', $response);
+        self::assertArrayHasKey('A2', $response);
+
+
+
+        self::assertEquals('Listed at Opencaching only', $response['A1']['name']);
+    }
+}

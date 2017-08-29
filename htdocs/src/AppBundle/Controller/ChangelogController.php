75b29184785d5218c9188b89bd99bef488aa24e5
Thoams Eiling <teiling88@gmail.com>
2017-08-29 23:37:18 +0200

    fix pr comments

--- htdocs/src/AppBundle/Controller/ChangelogController.php
+++ htdocs/src/AppBundle/Controller/ChangelogController.php
@@ -10,11 +10,11 @@ class ChangelogController extends Controller
 {
     /**
      * @Route("/changelog")
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
-    {   $converter = new CommonMarkConverter();
-        $changelog = $converter->convertToHtml(file_get_contents(__DIR__ . '/../../../../ChangeLog-3.1.md'));
+    {
+        $markConverter = $this->container->get('app.library.common_mark_converter');
+        $changelog = $markConverter->convertToHtml(file_get_contents(__DIR__ . '/../../../../ChangeLog-3.1.md'));
 
         return $this->render('AppBundle:ChangelogController:index.html.twig', ['changelog' => $changelog]);
     }

4275e10a9782e801f9a7635a4fc43b159967f9e5
following <following@online.de>
2017-09-13 23:33:17 +0200

    fix NUTS level 1 entity names and add missing SE NUTS 1/3 codes

--- bin/dbsv-update.php
+++ bin/dbsv-update.php
@@ -1224,6 +1224,27 @@ function dbv_163()
     update_triggers();
 }
 
+function dbv_164()
+{
+    global $opt;
+
+    // update NUTS codes
+    system(
+        'cat ' . __DIR__. '/../sql/static-data/nuts_codes.sql |' .
+        ' mysql -h' . $opt['db']['servername'] . ' -u' . $opt['db']['username'] .
+        ' --password=' . $opt['db']['password'] . ' ' . $opt['db']['placeholder']['db']
+    );
+
+    // update NUTS level 1 cache location names
+    sql(
+        "UPDATE
+            `cache_location` `cl`,
+            (SELECT `code`, `name` FROM `nuts_codes` WHERE `code` LIKE '___') AS `nc`
+         SET `adm2` = `nc`.`name`
+         WHERE `nc`.`code` = `cl`.`code2`
+         AND `cl`.`code1` != 'DE'"   // optimization - there are no changes for Germany
+    );
+}
 
 // When adding new mutations, take care that they behave well if run multiple
 // times. This improves robustness of database versioning.

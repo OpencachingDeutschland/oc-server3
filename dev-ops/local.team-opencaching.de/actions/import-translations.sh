463e7e4f9dbe561dd6c7b6c54a17d9b86d80639b
Thoams Eiling <teiling88@gmail.com>
2017-10-04 20:21:10 +0200

    fix import-translations.sh command

--- dev-ops/local.team-opencaching.de/actions/import-translations.sh
+++ dev-ops/local.team-opencaching.de/actions/import-translations.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #DESCRIPTION: import translation file from opencaching.de
 
-cd htdocs && curl -o translation.zip "https://www.opencaching.de/translations.zip"
-cd htdocs && unzip -o translation.zip
-cd htdocs && rm translation.zip
+cd htdocs && wget -O translations.zip "https://www.opencaching.de/translations.zip"
+cd htdocs && unzip -o translations.zip
+cd htdocs && rm translations.zip
 
 ./htdocs/bin/console translation:update de --force
 ./htdocs/bin/console translation:update el --force

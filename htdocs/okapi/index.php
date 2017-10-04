d87fcf53f697609dcd5393061d0df9aa3650d215
Thoams Eiling <teiling88@gmail.com>
2017-10-04 21:49:46 +0200

    update okapi to the latest version and remove packagist usage

--- htdocs/okapi/index.php
+++ htdocs/okapi/index.php
@@ -4,4 +4,4 @@ $GLOBALS['rootpath'] = __DIR__.'/../';
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ .'/../vendor/opencaching/okapi/index.php';
+require_once __DIR__ .'/../vendor/opencaching/okapi/okapi/index.php';

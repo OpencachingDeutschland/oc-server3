43b75961522499d9e9d6be1fedc066b2ada613ad
Thoams Eiling <teiling88@gmail.com>
2017-10-07 13:03:34 +0200

    fix code style

--- htdocs/okapi/meta.php
+++ htdocs/okapi/meta.php
@@ -1,4 +1,4 @@
-<?php return array (
-  'version_number' => 1663,
-  'git_revision' => 'c6a37a7f94d637525da29666e5b1df1229516204',
-);
+<?php return [
+    'version_number' => 1663,
+    'git_revision' => 'c6a37a7f94d637525da29666e5b1df1229516204',
+];

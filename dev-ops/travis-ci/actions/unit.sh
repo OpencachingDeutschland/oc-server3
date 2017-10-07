ae18f788abbadd1d1cd75847356977e7e799d06b
Thoams Eiling <teiling88@gmail.com>
2017-10-07 14:37:26 +0200

    activate codev report upload

--- dev-ops/travis-ci/actions/unit.sh
+++ dev-ops/travis-ci/actions/unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-URL=__FRONTEND_URL__ htdocs/vendor/phpunit/phpunit/phpunit --stop-on-failure --stop-on-error --debug --verbose # --coverage-html=build/artifacts/html-coverage
+URL=__FRONTEND_URL__ htdocs/vendor/phpunit/phpunit/phpunit --stop-on-failure --stop-on-error --debug --verbose --coverage-clover=coverage.xml

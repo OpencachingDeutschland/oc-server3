fa4a4133390382f4c3dcfa0d3ef4507a1c1663f2
Thoams Eiling <teiling88@gmail.com>
2017-10-17 23:42:41 +0200

    adjust some code styles

--- bin/upload-crowdin.php
+++ bin/upload-crowdin.php
@@ -1,17 +1,17 @@
 <?php
-$post_params = [];
+$postParams = [];
 $projectIdentifier = 'opencaching';
 $projectKey = getenv('crowdinApiKey');
-$request_url = 'https://api.crowdin.com/api/project/'.$projectIdentifier.'/update-file?key='.$projectKey;
+$requestUrl = 'https://api.crowdin.com/api/project/' . $projectIdentifier . '/update-file?key=' . $projectKey;
 
-$post_params['files[constants.en.yml]'] = curl_file_create(__DIR__.'/../htdocs/app/Resources/translation_source/constants.en.yml');
-$post_params['files[messages.en.yml]'] = curl_file_create(__DIR__.'/../htdocs/app/Resources/translation_source/messages.en.yml');
+$postParams['files[constants.en.yml]'] = curl_file_create(__DIR__.'/../htdocs/app/Resources/translation_source/constants.en.yml');
+$postParams['files[messages.en.yml]'] = curl_file_create(__DIR__.'/../htdocs/app/Resources/translation_source/messages.en.yml');
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $request_url);
+curl_setopt($ch, CURLOPT_URL, $requestUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post_params);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postParams);
 
 $result = curl_exec($ch);
 curl_close($ch);

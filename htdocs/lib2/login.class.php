8a2d205553e4cd8c3c37f24fe95f3c23feda6042
Thoams Eiling <teiling88@gmail.com>
2017-10-23 23:53:40 +0200

    fix if statement - contributed by @following5 during pr #683

--- htdocs/lib2/login.class.php
+++ htdocs/lib2/login.class.php
@@ -157,10 +157,12 @@ class login
             $min_lastlogin
         );
 
+        // sys_session.last_login controls the automatic logout of users at the OC website.
+        // user.last_login gives the overall last login date, including OKAPI logins.
 
         if ($rUser = sql_fetch_assoc($rs)) {
-            if ((($this->permanent == true) && (strtotime($rUser['last_login']) + LOGIN_TIME / 2 < time())) ||
-                (($this->permanent == false) && (strtotime($rUser['last_login']) + LOGIN_TIME_PERMANENT / 2 < time()))
+            if ((($this->permanent == true) && (strtotime($rUser['last_login']) + LOGIN_TIME_PERMANENT / 2 < time())) ||
+                (($this->permanent == false) && (strtotime($rUser['last_login']) + LOGIN_TIME / 2 < time()))
             ) {
                 sqlf(
                     "UPDATE `sys_sessions` SET `sys_sessions`.`last_login`=NOW()

7abab3cc5ca22dab70483e66aebdb8f3aa6c1b6c
Thoams Eiling <teiling88@gmail.com>
2017-11-03 21:52:59 +0100

    switch api/email_problems.php to dbal connection

--- htdocs/api/email_problems.php
+++ htdocs/api/email_problems.php
@@ -3,17 +3,20 @@
  * for license information see LICENSE.md
  ***************************************************************************/
 
+use Doctrine\DBAL\Connection;
+
 require __DIR__ . '/../lib2/web.inc.php';
+
+/** @var Connection $connection */
+$connection = AppKernel::Container()->get(Connection::class);
 
 header('Content-type: text/plain; charset=utf-8');
 
-if (isset($_REQUEST['key']) &&
-    $opt['logic']['api']['email_problems']['key'] &&
-    $opt['logic']['api']['email_problems']['key'] == $_REQUEST['key']
+if (isset($_REQUEST['key'], $opt['logic']['api']['email_problems']['key']) &&
+    $opt['logic']['api']['email_problems']['key'] === $_REQUEST['key']
 ) {
-    $rs = sql('SELECT `user_id`, `email_problems` FROM `user` WHERE `email_problems`');
-    while ($r = sql_fetch_assoc($rs)) {
+    $rs = $connection->fetchAssoc('SELECT `user_id`, `email_problems` FROM `user` WHERE `email_problems`');
+    foreach ($rs as $r) {
         echo $r['user_id'] . ' ' . $r['email_problems'] . "\n";
     }
-    sql_free_result($rs);
 }

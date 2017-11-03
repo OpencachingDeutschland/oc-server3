f3fe2f9895eee8e5807beaaf6a3ab45d45e1c721
Thoams Eiling <teiling88@gmail.com>
2017-11-03 22:07:33 +0100

    switch api/user_inactivity.php to dbal connection

--- htdocs/api/user_inactivity.php
+++ htdocs/api/user_inactivity.php
@@ -3,23 +3,26 @@
  * for license information see LICENSE.md
  ***************************************************************************/
 
+use Doctrine\DBAL\Connection;
+
 require __DIR__ . '/../lib2/web.inc.php';
+
+/** @var Connection $connection */
+$connection = AppKernel::Container()->get(Connection::class);
 
 header('Content-type: text/plain; charset=utf-8');
 
-if ($opt['logic']['api']['user_inactivity']['key']
-    && isset($_REQUEST['key'])
-    && $opt['logic']['api']['user_inactivity']['key'] == $_REQUEST['key']
-    && isset($_REQUEST['userid'])
+if (isset($_REQUEST['key'], $opt['logic']['api']['user_inactivity']['key'], $_REQUEST['userid'])
+    && $opt['logic']['api']['user_inactivity']['key'] === $_REQUEST['key']
 ) {
-    $loginLag = sql_value(
-        "SELECT DATEDIFF(NOW(),`last_login`)
+    $loginLag = $connection->fetchColumn(
+        'SELECT DATEDIFF(NOW(),`last_login`)
          FROM `user`
-         WHERE `user_id`='&1'",
-        null,
-        $_REQUEST['userid']
+         WHERE `user_id`= :userId',
+        [':userId' => $_REQUEST['userid']]
     );
-    if ($loginLag !== null) {
+
+    if ($loginLag) {
         echo floor($loginLag / 30.5);
     } else {
         echo 'unknown';

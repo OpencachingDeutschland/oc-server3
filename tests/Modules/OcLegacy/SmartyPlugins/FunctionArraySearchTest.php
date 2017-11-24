6e46390e61e4e25eb01b280b27efc402bb4d16d1
Thomas Eiling <teiling88@gmail.com>
2017-11-24 14:05:34 +0100

    add FunctionArraySearchTest.php

---
+++ tests/Modules/OcLegacy/SmartyPlugins/FunctionArraySearchTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OcTest\Modules\OcLegacy\SmartyPlugins;
+
+use OcTest\Modules\AbstractModuleTest;
+use OcTest\Utils\SmartyDummy;
+
+require_once __DIR__ . '/../../../../htdocs/src/OcLegacy/SmartyPlugins/function.array_search.php';
+
+class FunctionArraySearchTest extends AbstractModuleTest
+{
+    public function test_array_search_returns_empty_string()
+    {
+        $smarty = new SmartyDummy();
+        self::assertEquals(
+            '',
+            \smarty_function_array_search(['haystack' => false, 'var' => 'test'], $smarty)
+        );
+
+        self::assertEquals(
+            '',
+            \smarty_function_array_search(
+                ['haystack' => ['test123','searching'], 'needle' => 'searching', 'var' => 'test'],
+                $smarty
+            )
+        );
+    }
+}

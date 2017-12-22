e859f5a0f7ca942ee5dc5e9e763b8ba7b6e1bfa0
Thomas Eiling <teiling88@gmail.com>
2017-12-22 21:30:46 +0100

    activate PHPUnit Result Printer

--- dev-ops/local.team-opencaching.de/actions/unit-fast.sh
+++ dev-ops/local.team-opencaching.de/actions/unit-fast.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: executes unit tests
 
-phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --debug --verbose
+phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error
--- dev-ops/local.team-opencaching.de/actions/unit.sh
+++ dev-ops/local.team-opencaching.de/actions/unit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: executes unit tests with code coverage
 
-phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --debug --verbose --coverage-html=build/artifacts/html-coverage
+phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --coverage-html=build/artifacts/html-coverage

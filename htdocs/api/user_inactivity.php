OpencachingDeutschland/oc-server3

commit f3fe2f9895eee8e5807beaaf6a3ab45d45e1c721

switch api/user_inactivity.php to dbal connection

htdocs/api/user_inactivity.php
@@ -3,23 +3,26 @@
  * for license information see LICENSE.md
  ***************************************************************************/
 
+use Doctrine\DBAL\Connection;
+
 require __DIR__ . '/../lib2/web.inc.php';
+
+/** @var Connection $connection */
+$connection = AppKernel::Container()->get(Connection::class);
 
 header('Content-type: text/plain; charset=utf-8');
 
-if ($opt['logic']['api']['user_inactivity']['key']
-    && isset($_REQUEST['key'])
-    && $opt['logic']['api']['user_inactivity']['key'] == $_REQUEST['key']
-    && isset($_REQUEST['userid'])
+if (isset($_REQUEST['key'], $opt['logic']['api']['user_inactivity']['key'], $_REQUEST['userid'])
+    && $opt['logic']['api']['user_inactivity']['key'] === $_REQUEST['key']
 ) {
-    $loginLag = sql_value(
-        "SELECT DATEDIFF(NOW(),`last_login`)
+    $loginLag = $connection->fetchColumn(
+        'SELECT DATEDIFF(NOW(),`last_login`)
          FROM `user`
-         WHERE `user_id`='&1'",
-        null,
-        $_REQUEST['userid']
+         WHERE `user_id`= :userId',
+        [':userId' => $_REQUEST['userid']]
     );
-    if ($loginLag !== null) {
+
+    if ($loginLag) {
         echo floor($loginLag / 30.5);
     } else {
         echo 'unknown';

commit 193ecce812ac746d54b36b21a054f9bdc3143a6d

fix user_inactivity api bug introduced with f3fe2f9

htdocs/api/user_inactivity.php
@@ -22,7 +22,7 @@ if (isset($_REQUEST['key'], $opt['logic']['api']['user_inactivity']['key'], $_RE
         [':userId' => $_REQUEST['userid']]
     );
 
-    if ($loginLag) {
+    if ($loginLag !== null) {
         echo floor($loginLag / 30.5);
     } else {
         echo 'unknown';
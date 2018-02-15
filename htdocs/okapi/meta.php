e8430e9ad934b22f6dea9cd5f5c4aace76916ae4
Thomas Eiling <teiling88@gmail.com>
2018-02-15 21:25:27 +0000

    update okapi package

--- htdocs/okapi/meta.php
+++ htdocs/okapi/meta.php
@@ -1,4 +1,4 @@
 <?php return array (
-  'version_number' => 1686,
-  'git_revision' => 'f899783a15b8b8721da9792c94314a2b462f05e7',
+  'version_number' => 1687,
+  'git_revision' => 'a18bee1a2c15f4bea9015ee9648ff8e46adff809',
 );

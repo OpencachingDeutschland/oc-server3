acc00d4eb0f69da3b137dec14b5cbd42759e8db2
Thomas Eiling <teiling88@gmail.com>
2018-08-12 19:28:41 +0200

    fix email_problems.php

--- htdocs/api/email_problems.php
+++ htdocs/api/email_problems.php
@@ -15,7 +15,7 @@ header('Content-type: text/plain; charset=utf-8');
 if (isset($_REQUEST['key'], $opt['logic']['api']['email_problems']['key']) &&
     $opt['logic']['api']['email_problems']['key'] === $_REQUEST['key']
 ) {
-    $rs = $connection->fetchAssoc('SELECT `user_id`, `email_problems` FROM `user` WHERE `email_problems`');
+    $rs = $connection->fetchAll('SELECT `user_id`, `email_problems` FROM `user` WHERE `email_problems`');
     foreach ($rs as $r) {
         echo $r['user_id'] . ' ' . $r['email_problems'] . "\n";
     }

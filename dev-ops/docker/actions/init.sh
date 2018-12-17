971a0f0f44678344050691c933d7444c5c50705d
Kai Neuwerth <kai.neuwerth@netshops.de>
2018-12-17 16:00:26 +0100

    Remove sudo from chmod in docker psh init script

--- dev-ops/docker/actions/init.sh
+++ dev-ops/docker/actions/init.sh
@@ -9,4 +9,4 @@ INCLUDE: ./sf-migrations.sh
 INCLUDE: ../../local.team-opencaching.de/actions/okapi-update.sh
 INCLUDE: ../../local.team-opencaching.de/actions/import-translations.sh
 
-I: cd /usr/local/bin && ln -sf /application/htdocs/vendor/phpunit/phpunit/phpunit && sudo chmod 755 phpunit
+I: cd /usr/local/bin && ln -sf /application/htdocs/vendor/phpunit/phpunit/phpunit && chmod 755 phpunit

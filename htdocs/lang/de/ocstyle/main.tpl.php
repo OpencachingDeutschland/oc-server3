9c923f719e23110743aed77466712730def05c57
Thomas Eiling <teiling88@gmail.com>
2018-12-27 11:07:57 +0100

    fix privacy statement link

--- htdocs/lang/de/ocstyle/main.tpl.php
+++ htdocs/lang/de/ocstyle/main.tpl.php
@@ -383,7 +383,7 @@ foreach ($opt['template']['locales'] as $k => $lang) {
 
                 <!-- FOOTER -->
                 <div class="footer">
-                    <p><a href="articles.php?page=dsb">{t}Privacy statement{/t}</a> | <a href="articles.php?page=impressum">{t}Terms of use and legal information{/t}</a> | <a href="articles.php?page=contact">{t}Contact{/t}</a> | <a href="articles.php?page=changelog">{t}Changelog{/t}</a> | <a href="sitemap.php">{t}Sitemap{/t}</a></p>
+                    <p><a href="/page/datenschutzerklaerung">{t}Privacy statement{/t}</a> | <a href="articles.php?page=impressum">{t}Terms of use and legal information{/t}</a> | <a href="articles.php?page=contact">{t}Contact{/t}</a> | <a href="articles.php?page=changelog">{t}Changelog{/t}</a> | <a href="sitemap.php">{t}Sitemap{/t}</a></p>
                     <p><strong>{sponsorbottom}</strong></p>
                 </div>
 

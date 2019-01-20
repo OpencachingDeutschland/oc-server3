067d7d6acb68bbcacdfbc2c3729e2122f2140e68
Thomas Eiling <teiling88@gmail.com>
2019-01-20 23:07:56 +0100

    update okapi

--- htdocs/okapi/meta.php
+++ htdocs/okapi/meta.php
@@ -1,4 +1,4 @@
 <?php return array (
-  'version_number' => 1874,
-  'git_revision' => '57d4c7a2803a0d7784ae2dc994d0708eb5145bd5',
+  'version_number' => 1895,
+  'git_revision' => '2d611228f872e31c5ccc6a814b3f24b82014ae41',
 );

6aa44f3a9a9c8064bfc4477c33aa6e6e2fe6a4ce
Thomas Eiling <teiling88@gmail.com>
2019-09-17 13:14:56 +0200

    update travis setup

--- dev-ops/travis-ci/actions/code-style.sh
+++ dev-ops/travis-ci/actions/code-style.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -i -u __USERKEY__ -t __PHP_FPM_ID__ ./php-cs-fixer fix --dry-run --using-cache=no
+docker exec opencaching-php-fpm ./php-cs-fixer fix --dry-run --using-cache=no
--- dev-ops/travis-ci/actions/unit.sh
+++ dev-ops/travis-ci/actions/unit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -i -u __USERKEY__ -t __PHP_FPM_ID__ URL=__FRONTEND_URL__ htdocs/vendor/phpunit/phpunit/phpunit --stop-on-failure --stop-on-error --debug --verbose --coverage-clover=coverage.xml
+docker exec opencaching-php-fpm URL=__FRONTEND_URL__ htdocs/vendor/phpunit/phpunit/phpunit --stop-on-failure --stop-on-error --debug --verbose --coverage-clover=coverage.xml

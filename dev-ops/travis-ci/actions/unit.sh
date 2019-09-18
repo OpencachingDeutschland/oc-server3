a7bd35d8c19faf2a0959622851788c2f92876a37
Thomas Eiling <teiling88@gmail.com>
2019-09-18 13:28:39 +0200

    update travis setup

--- dev-ops/travis-ci/actions/unit.sh
+++ dev-ops/travis-ci/actions/unit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #DESCRIPTION: executes unit tests with code coverage
 
-URL=http://localhost ./htdocs/vendor/bin/phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --coverage-html=build/artifacts/html-coverage
+URL=__FRONTEND_URL__ ./htdocs/vendor/bin/phpunit -c phpunit.xml.dist --stop-on-failure --stop-on-error --coverage-html=build/artifacts/html-coverage

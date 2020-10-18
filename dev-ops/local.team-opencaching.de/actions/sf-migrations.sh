c3e10306693041dd86aa7f1eac181dc7df9da69e
Nick Lubisch <github@nicklubisch.de>
2020-10-18 21:08:17 +0200

    OCC-71: Rename htdocs2 to htdocs_symfony

--- dev-ops/local.team-opencaching.de/actions/sf-migrations.sh
+++ dev-ops/local.team-opencaching.de/actions/sf-migrations.sh
@@ -5,6 +5,6 @@ chmod 755 ./htdocs/bin/console
 sudo chmod -R 777 ./htdocs/var
 ./htdocs/bin/console doctrine:migrations:migrate -n
 
-chmod 755 ./htdocs2/bin/console
-sudo chmod -R 777 ./htdocs2/var
-./htdocs2/bin/console doctrine:migrations:migrate -n
+chmod 755 ./htdocs_symfony/bin/console
+sudo chmod -R 777 ./htdocs_symfony/var
+./htdocs_symfony/bin/console doctrine:migrations:migrate -n

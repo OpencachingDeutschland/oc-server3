a5ad3ca22a95083abd259aa4f484e4d6fe87dc61
fraggle-DE <b223@gmx.de>
2021-04-27 18:05:38 +0200

    finished cache search function

--- htdocs_symfony/src/Controller/Backend/SupportController.php
+++ htdocs_symfony/src/Controller/Backend/SupportController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Oc\Controller\Backend;
 
 use Doctrine\DBAL\Connection;
+use Oc\Form\SupportSearchCaches;
 use Oc\Form\SupportSQLFlexForm;
 use Oc\Repository\CacheReportsRepository;
 use Oc\Repository\CacheStatusModifiedRepository;
@@ -64,13 +65,35 @@ class SupportController extends AbstractController
     }
 
     /**
+     * @param Request $request
+     *
      * @return Response
      * @Route("/supportSearch", name="support_search")
      */
-    public function serchCachesAndUser()
-    : Response
-    {
-        return $this->render('backend/support/KOMMTDEMNAECHST.html.twig');
+    public function searchCachesAndUser(Request $request)
+    : Response {
+        $fetchedCaches = '';
+        $limit = false;
+
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
+        $formSearch->handleRequest($request);
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $inputData = $formSearch->getData();
+
+            if ($formSearch->getClickedButton() === $formSearch->get('search_One')) {
+                $limit = true;
+            }
+
+            $fetchedCaches = $this->getCachesForSearchField($inputData['content_support_searchfield'], $limit);
+        }
+
+        return $this->render(
+            'backend/support/searchedCaches.html.twig', [
+                                                          'supportCachesForm' => $formSearch->createView(),
+                                                          'foundCaches' => $fetchedCaches
+                                                      ]
+        );
     }
 
     /**
@@ -83,7 +106,14 @@ class SupportController extends AbstractController
     {
         $fetchedReports = $this->getReportedCaches();
 
-        return $this->render('backend/support/reportedCaches.html.twig', ['reportedCaches_by_id' => $fetchedReports]);
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
+        return $this->render(
+            'backend/support/reportedCaches.html.twig', [
+                                                          'supportCachesForm' => $formSearch->createView(),
+                                                          'reportedCaches_by_id' => $fetchedReports
+                                                      ]
+        );
     }
 
     /**
@@ -96,6 +126,8 @@ class SupportController extends AbstractController
     : Response {
         $fetchedInformation = [];
 
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
         $form = $this->createForm(SupportSQLFlexForm::class);
 
         $form->handleRequest($request);
@@ -106,7 +138,7 @@ class SupportController extends AbstractController
             $fetchedInformation = $this->executeSQL_flexible($inputData['content_WHAT'], $inputData['content_TABLE']);
 
             $countFetched = count($fetchedInformation);
-            for ($i = 0; $i < $countFetched; $i++) {
+            for ($i = 0; $i < $countFetched; $i ++) {
                 if (array_key_exists('password', $fetchedInformation[$i])) {
                     $fetchedInformation[$i]['password'] = '-';
                 }
@@ -117,7 +149,11 @@ class SupportController extends AbstractController
         }
 
         return $this->render(
-            'backend/support/databaseQueries.html.twig', ['SQLFlexForm' => $form->createView(), 'suppSQLqueryFlex' => $fetchedInformation]
+            'backend/support/databaseQueries.html.twig', [
+                                                           'supportCachesForm' => $formSearch->createView(),
+                                                           'SQLFlexForm' => $form->createView(),
+                                                           'suppSQLqueryFlex' => $fetchedInformation
+                                                       ]
         );
     }
 
@@ -131,6 +167,8 @@ class SupportController extends AbstractController
      */
     public function list_reported_cache_details(string $repID)
     : Response {
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
         $fetchedReport = $this->cacheReportsRepository->fetchOneBy(['id' => $repID]);
 
         $fetchedStatus = $this->cacheStatusRepository->fetchAll();
@@ -139,11 +177,51 @@ class SupportController extends AbstractController
 
         return $this->render(
             'backend/support/reportedCacheDetails.html.twig', [
+                                                                'supportCachesForm' => $formSearch->createView(),
                                                                 'reported_cache_by_id' => $fetchedReport,
                                                                 'cache_status' => $fetchedStatus,
                                                                 'report_status_modified' => $fetchedStatusModfied
                                                             ]
         );
+    }
+
+    /**
+     * @param string $searchtext
+     * @param bool $limit
+     *
+     * @return array
+     */
+    public function getCachesForSearchField(string $searchtext, bool $limit = false)
+    : array {
+        //      so sieht die SQL-Vorlage aus..
+        //        SELECT name, wp_oc, wp_gc, wp_gc_maintained, user.username, user.email
+        //        FROM caches
+        //        INNER JOIN user ON caches.user_id = user.user_id
+        //        WHERE wp_oc         =       "' . $searchtext . '"
+        //        OR wp_gc            =       "' . $searchtext . '"
+        //        OR wp_gc_maintained =       "' . $searchtext . '"
+        //        OR caches.name     LIKE    "%' . $searchtext . '%"'
+        //        OR user.username   LIKE    "%' . $searchtext . '%"'
+        //        OR user.email      LIKE    "%' . $searchtext . '%"'
+        //        LIMIT $limit
+        $qb = $this->connection->createQueryBuilder();
+        $qb->select('caches.name', 'caches.wp_oc', 'caches.wp_gc', 'caches.wp_gc_maintained', 'user.username', 'user.email')
+            ->from('caches')
+            ->innerJoin('caches', 'user', 'user', 'caches.user_id = user.user_id')
+            ->where('caches.wp_oc = :searchTerm')
+            ->orWhere('caches.wp_gc = :searchTerm')
+            ->orWhere('caches.wp_gc_maintained = :searchTerm')
+            ->orWhere('caches.name LIKE :searchTermLIKE')
+            ->orWhere('user.username LIKE :searchTermLIKE')
+            ->orWhere('user.email LIKE :searchTermLIKE')
+            ->setParameters(['searchTerm' => $searchtext, 'searchTermLIKE' => '%' . $searchtext . '%'])
+            ->orderBy('caches.wp_oc', 'ASC');
+
+        if ($limit === true) {
+            $qb->setMaxResults(1);
+        }
+
+        return $qb->execute()->fetchAll();
     }
 
     /**
@@ -165,6 +243,8 @@ class SupportController extends AbstractController
     public function executeSQL_caches_old_reg_date(int $days = 31) // List caches from users whose registration date is not older than x days.
     : Response
     {
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
         $qb = $this->connection->createQueryBuilder();
         $qb->select('caches.name', 'user.username', 'user.date_created', 'user.last_login')
             ->from('caches')
@@ -174,7 +254,12 @@ class SupportController extends AbstractController
             ->setParameters(['searchTerm' => $days])
             ->orderBy('date_created', 'DESC');
 
-        return $this->render('backend/support/databaseQueries.html.twig', ['suppSQLquery1' => $qb->execute()->fetchAll()]);
+        return $this->render(
+            'backend/support/databaseQueries.html.twig', [
+                                                           'supportCachesForm' => $formSearch->createView(),
+                                                           'suppSQLquery1' => $qb->execute()->fetchAll()
+                                                       ]
+        );
     }
 
     /**
@@ -186,6 +271,8 @@ class SupportController extends AbstractController
     public function executeSQL_old_reg_date(int $days) // List user whose registration date is no older than x days.
     : Response
     {
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
         $qb = $this->connection->createQueryBuilder();
         $qb->select('username', 'date_created', 'last_login')
             ->from('user')
@@ -193,7 +280,12 @@ class SupportController extends AbstractController
             ->setParameters(['searchTerm' => $days])
             ->orderBy('date_created', 'DESC');
 
-        return $this->render('backend/support/databaseQueries.html.twig', ['suppSQLquery2' => $qb->execute()->fetchAll()]);
+        return $this->render(
+            'backend/support/databaseQueries.html.twig', [
+                                                           'supportCachesForm' => $formSearch->createView(),
+                                                           'suppSQLquery2' => $qb->execute()->fetchAll()
+                                                       ]
+        );
     }
 
     /**
@@ -204,6 +296,8 @@ class SupportController extends AbstractController
     ) // List (non-archived, non-locked) caches from users whose last login date is older than one year.
     : Response
     {
+        $formSearch = $this->createForm(SupportSearchCaches::class);
+
         $qb = $this->connection->createQueryBuilder();
         $qb->select('caches.name', 'caches.cache_id', 'caches.status', 'user.username', 'user.last_login')
             ->from('caches')
@@ -214,7 +308,12 @@ class SupportController extends AbstractController
             ->setParameters(['searchTerm' => 1])
             ->orderBy('user.last_login', 'ASC');
 
-        return $this->render('backend/support/databaseQueries.html.twig', ['suppSQLquery4' => $qb->execute()->fetchAll()]);
+        return $this->render(
+            'backend/support/databaseQueries.html.twig', [
+                                                           'supportCachesForm' => $formSearch->createView(),
+                                                           'suppSQLquery4' => $qb->execute()->fetchAll()
+                                                       ]
+        );
     }
 
     /**

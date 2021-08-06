32fa114a71ec332f0918f99061f7f53d5c571b89
fraggle-DE <b223@gmx.de>
2021-08-06 21:20:09 +0200

    added Repositories

---
+++ htdocs_symfony/src/Repository/SupportListingInfosRepository.php
@@ -0,0 +1,250 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oc\Repository;
+
+use DateTime;
+use Doctrine\DBAL\Connection;
+use Oc\Entity\SupportListingInfosEntity;
+use Oc\Repository\Exception\RecordAlreadyExistsException;
+use Oc\Repository\Exception\RecordNotFoundException;
+use Oc\Repository\Exception\RecordNotPersistedException;
+use Oc\Repository\Exception\RecordsNotFoundException;
+
+/**
+ * Class SupportListingInfosRepository
+ *
+ * @package Oc\Repository
+ */
+class SupportListingInfosRepository
+{
+    const TABLE = 'support_listing_comments';
+
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @return array
+     * @throws RecordsNotFoundException
+     */
+    public function fetchAll()
+    : array
+    {
+        $statement = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from(self::TABLE)
+            ->execute();
+
+        $result = $statement->fetchAll();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordsNotFoundException('No records found');
+        }
+
+        $records = [];
+
+        foreach ($result as $item) {
+            $records[] = $this->getEntityFromDatabaseArray($item);
+        }
+
+        return $records;
+    }
+
+    /**
+     * @param array $where
+     *
+     * @return SupportListingInfosEntity
+     * @throws RecordNotFoundException
+     */
+    public function fetchOneBy(array $where = [])
+    : SupportListingInfosEntity {
+        $queryBuilder = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from(self::TABLE)
+            ->setMaxResults(1);
+
+        if (count($where) > 0) {
+            foreach ($where as $column => $value) {
+                $queryBuilder->andWhere($column . ' = ' . $queryBuilder->createNamedParameter($value));
+            }
+        }
+
+        $statement = $queryBuilder->execute();
+
+        $result = $statement->fetch();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordNotFoundException('Record with given where clause not found');
+        }
+
+        return $this->getEntityFromDatabaseArray($result);
+    }
+
+    /**
+     * @param array $where
+     *
+     * @return array
+     * @throws RecordsNotFoundException
+     */
+    public function fetchBy(array $where = [])
+    : array {
+        $queryBuilder = $this->connection->createQueryBuilder()
+            ->select('*')
+            ->from(self::TABLE);
+
+        if (count($where) > 0) {
+            foreach ($where as $column => $value) {
+                $queryBuilder->andWhere($column . ' = ' . $queryBuilder->createNamedParameter($value));
+            }
+        }
+
+        $statement = $queryBuilder->execute();
+
+        $result = $statement->fetchAll();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordsNotFoundException('No records with given where clause found');
+        }
+
+        $entities = [];
+
+        foreach ($result as $item) {
+            $entities[] = $this->getEntityFromDatabaseArray($item);
+        }
+
+        return $entities;
+    }
+
+    /**
+     * @param SupportListingInfosEntity $entity
+     *
+     * @return SupportListingInfosEntity
+     * @throws RecordAlreadyExistsException
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function create(SupportListingInfosEntity $entity)
+    : SupportListingInfosEntity {
+        if (!$entity->isNew()) {
+            throw new RecordAlreadyExistsException('The entity does already exist.');
+        }
+
+        $databaseArray = $this->getDatabaseArrayFromEntity($entity);
+
+        $this->connection->insert(
+            self::TABLE,
+            $databaseArray
+        );
+
+        $entity->id = (int) $this->connection->lastInsertId();
+
+        return $entity;
+    }
+
+    /**
+     * @param SupportListingInfosEntity $entity
+     *
+     * @return SupportListingInfosEntity
+     * @throws RecordNotPersistedException
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function update(SupportListingInfosEntity $entity)
+    : SupportListingInfosEntity {
+        if ($entity->isNew()) {
+            throw new RecordNotPersistedException('The entity does not exist.');
+        }
+
+        $databaseArray = $this->getDatabaseArrayFromEntity($entity);
+
+        $this->connection->update(
+            self::TABLE,
+            $databaseArray,
+            ['id' => $entity->id]
+        );
+
+        return $entity;
+    }
+
+    /**
+     * @param SupportListingInfosEntity $entity
+     *
+     * @return SupportListingInfosEntity
+     * @throws RecordNotPersistedException
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Exception\InvalidArgumentException
+     */
+    public function remove(SupportListingInfosEntity $entity)
+    : SupportListingInfosEntity {
+        if ($entity->isNew()) {
+            throw new RecordNotPersistedException('The entity does not exist.');
+        }
+
+        $this->connection->delete(
+            self::TABLE,
+            ['id' => $entity->id]
+        );
+
+        $entity->id = null;
+
+        return $entity;
+    }
+
+    /**
+     * @param SupportListingInfosEntity $entity
+     *
+     * @return array
+     */
+    public function getDatabaseArrayFromEntity(SupportListingInfosEntity $entity)
+    : array {
+        return [
+            'id' => $entity->id,
+            'wp_oc' => $entity->wpOc,
+            'node_id' => $entity->nodeId,
+            'node_owner_id' => $entity->nodeOwnerId,
+            'node_listing_id' => $entity->nodeListingId,
+            'node_listing_wp' => $entity->nodeListingWP,
+            'node_listing_name' => $entity->nodeListingName,
+            'node_listing_size' => $entity->nodeListingSize,
+            'node_listing_difficulty' => $entity->nodeListingDifficulty,
+            'node_listing_terrain' => $entity->nodeListingTerrain,
+            'node_listing_lon' => $entity->nodeListingLon,
+            'node_listing_lat' => $entity->nodeListingLat,
+            'node_listing_archived' => $entity->nodeListingArchived,
+            'node_listing_lastmodified' => $entity->lastModified,
+            'import_status' => $entity->importStatus,
+        ];
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return SupportListingInfosEntity
+     * @throws \Exception
+     */
+    public function getEntityFromDatabaseArray(array $data)
+    : SupportListingInfosEntity {
+        $entity = new SupportListingInfosEntity();
+        $entity->id = (int) $data['id'];
+        $entity->wpOc = (string) $data['wp_oc'];
+        $entity->nodeId = (int) $data['node_id'];
+        $entity->nodeOwnerId = (string) $data['node_owner_id'];
+        $entity->nodeListingId = (string) $data['node_listing_id'];
+        $entity->nodeListingWP = (string) $data['node_listing_wp'];
+        $entity->nodeListingName = (string) $data['node_listing_name'];
+        $entity->nodeListingSize = (int) $data['node_listing_size'];
+        $entity->nodeListingDifficulty = (int) $data['node_listing_difficulty'];
+        $entity->nodeListingTerrain = (int) $data['node_listing_terrain'];
+        $entity->nodeListingLon = (double) $data['node_listing_lon'];
+        $entity->nodeListingLat = (double) $data['node_listing_lat'];
+        $entity->nodeListingArchived = (bool) $data['node_listing_archived'];
+        $entity->lastModified = new DateTime($data['node_listing_lastmodified']);
+        $entity->importStatus = (int) $data['import_status'];
+
+        return $entity;
+    }
+}

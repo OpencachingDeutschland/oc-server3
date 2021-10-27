379dc5f1da599390998dd37b10154e51d25d5d4c
fraggle-DE <b223@gmx.de>
2021-10-27 16:34:16 +0200

    finished

--- htdocs_symfony/src/Controller/Backend/OCOnly81Controller.php
+++ htdocs_symfony/src/Controller/Backend/OCOnly81Controller.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace Oc\Controller\Backend;
 
 use Doctrine\DBAL\Connection;
-use Oc\Repository\CachesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,27 +18,22 @@ class OCOnly81Controller extends AbstractController
 {
     private $connection;
 
-    private $cachesRepository;
-
     /**
      * CachesController constructor.
      *
      * @param Connection $connection
-     * @param CachesRepository $cachesRepository
      */
-    public function __construct(Connection $connection, CachesRepository $cachesRepository)
+    public function __construct(Connection $connection)
     {
         $this->connection = $connection;
-        $this->cachesRepository = $cachesRepository;
     }
 
     /**
-     * @param Request $request
      * @Route("/oconly81", name="oconly81_index")
      *
      * @return Response
      */
-    public function ocOnly81Controller_index(Request $request)
+    public function ocOnly81Controller_index()
     : Response {
         $userData = $this->ocOnly81_get_user_counts();
         $matrixData = $this->ocOnly81_get_matrixData();
@@ -48,18 +41,49 @@ class OCOnly81Controller extends AbstractController
         return $this->render(
             'backend/oconly81/index.html.twig', [
                                                   'ocOnly81_user' => $userData,
-                                                  'ocOnly81_matrix' => $matrixData
+                                                  'ocOnly81_matrix' => $matrixData[0],
+                                                  'ocOnly81_dsum' => $matrixData[1],
+                                                  'ocOnly81_tsum' => $matrixData[2],
+                                                  'ocOnly81_overall_sum' => $matrixData[3]
                                               ]
         );
     }
 
     /**
      * @return array
+     *
+     * OCOnly81 Datenbankabfrage: Verteilung der OCOnly-Caches in der 81er Matrix, sowie Summen der einzelnen Zeilen/Spalten erstellen
      */
     private function ocOnly81_get_matrixData()
     : array
     {
-        return [];
+        for ($i = 0; $i <= 8; $i ++) {
+            for ($j = 0; $j <= 8; $j ++) {
+                $matrix[$i][$j] = 0;
+                $dsum[$i] = 0;
+                $tsum[$i] = 0;
+            }
+        }
+
+        $qb = $this->connection->createQueryBuilder();
+        $qb->select('caches.difficulty', 'caches.terrain', 'COUNT(*) AS count')
+            ->from('caches')
+            ->innerJoin('caches', 'caches_attributes', 'caches_attributes', 'caches_attributes.cache_id = caches.cache_id AND caches_attributes.attrib_id = 6')
+            ->where('caches.status = 1')
+            ->groupBy('difficulty', 'terrain');
+
+        $data = $qb->execute()->fetchAll();
+
+        foreach ($data as $item) {
+            $matrix[$item['difficulty'] - 2][$item['terrain'] - 2] ++;
+        }
+
+        for ($i = 0; $i <= 8; $i ++) {
+            $dsum[$i] = $dsum[$i] + array_sum($matrix[$i]);
+            $tsum[$i] = $tsum[$i] + array_sum(array_column($matrix, $i));
+        }
+
+        return ([$matrix, $dsum, $tsum, array_sum($dsum)]);
     }
 
     /**
@@ -67,14 +91,14 @@ class OCOnly81Controller extends AbstractController
      *
      * @return array
      *
-     * OCOnly81-DB-Abfrage auswerten: Anzahl der OCOnly-Funde je Nutzer
+     * OCOnly81 Datenbankabfrage: Anzahl der OCOnly-Funde je Nutzer
      */
     private function ocOnly81_get_user_counts(int $limit = 0)
     : array {
         $result = [];
 
         $qb = $this->connection->createQueryBuilder();
-//        $qb->select('user.user_id', 'user.username', 'caches.difficulty', 'caches.terrain')
+        //        $qb->select('user.user_id', 'user.username', 'caches.difficulty', 'caches.terrain')
         $qb->select('user.user_id', 'user.username')
             ->from('user')
             ->innerJoin('user', 'cache_logs', 'cache_logs', 'cache_logs.user_id = user.user_id AND cache_logs.type = 1')

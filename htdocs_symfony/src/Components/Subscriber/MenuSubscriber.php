c3535852458540add7020f63cd92fea0647ec55e
fraggle-DE <b223@gmx.de>
2021-12-10 23:14:19 +0100

    Leaflet

--- htdocs_symfony/src/Components/Subscriber/MenuSubscriber.php
+++ htdocs_symfony/src/Components/Subscriber/MenuSubscriber.php
@@ -84,6 +84,14 @@ class MenuSubscriber implements EventSubscriberInterface
         }
 
         if ($this->security->isGranted("CAN_VIEW", UserEntity::class)) {
+            $menu->addChild('maps', [
+                'label' => 'Maps',
+                'route' => 'backend_map_single',
+                'childOptions' => $event->getChildOptions(),
+            ])->setLabelAttribute('icon', 'fas fa-map');
+        }
+
+        if ($this->security->isGranted("CAN_VIEW", UserEntity::class)) {
             $menu->addChild('kitchensink', [
                 'label' => 'Kitchensink',
                 'route' => 'app_kitchensink_index',

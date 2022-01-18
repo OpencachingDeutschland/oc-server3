0827c7606853767f4832d48e5f4d26e034d15201
fraggle-DE <b223@gmx.de>
2022-01-18 19:16:17 +0100

    index action

--- htdocs_symfony/src/Controller/Backend/UserController.php
+++ htdocs_symfony/src/Controller/Backend/UserController.php
@@ -4,20 +4,29 @@ declare(strict_types=1);
 
 namespace Oc\Controller\Backend;
 
-use Oc\Entity\UserEntity;
+use Oc\Form\CachesFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ */
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user_index")
+     * @Security("is_granted('ROLE_TEAM')")
      */
-    public function index(): Response
+    public function index()
+    : Response
     {
-        $this->denyAccessUnlessGranted('CAN_VIEW', UserEntity::class);
+        $searchForm = $this->createForm(CachesFormType::class);
 
-        return $this->render('backend/user/index.html.twig');
+        return $this->render('backend/user/index.html.twig', [
+                                                               'userSearchForm' => $searchForm->createView(),
+                                                           ]
+        );
     }
 }

ed72988209632b0d7031902091925536c6674c74
salmiaker <salmiaker@gmx.de>
2022-04-15 11:25:57 +0200

    removed previous version of find-old.sh

--- find-old.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep -R --exclude-dir=htdocs/vendor --exclude-dir=htdocs/app/Resources/translations --exclude-dir=htdocs/var/cache2/smarty/compiled --exclude-dir=htdocs/var/cache2/smarty/cache -n --color=auto $1 htdocs/*

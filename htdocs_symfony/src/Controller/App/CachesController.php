d1190ebd5c0a86348ffa01f6760b630fa06e9b72
fraggle-DE <b223@gmx.de>
2022-10-05 19:28:44 +0200

    double controller

---
+++ htdocs_symfony/src/Controller/App/CachesController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oc\Controller\App;
+
+/**
+ * Class CachesController
+ *
+ * @package Oc\Controller\Backend
+ */
+class CachesController extends \Oc\Controller\Backend\CachesController
+{
+}

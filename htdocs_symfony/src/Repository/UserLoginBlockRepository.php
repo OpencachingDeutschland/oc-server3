b8bae5a710d1aef195f26c1ae391e1d25154f983
fraggle-DE <b223@gmx.de>
2023-04-05 21:30:06 +0200

    add, change and remove user login block

---
+++ htdocs_symfony/src/Repository/UserLoginBlockRepository.php
@@ -0,0 +1,198 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oc\Repository;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception\InvalidArgumentException;
+use Exception;
+use Oc\Entity\UserLoginBlockEntity;
+use Oc\Repository\Exception\RecordAlreadyExistsException;
+use Oc\Repository\Exception\RecordNotFoundException;
+use Oc\Repository\Exception\RecordNotPersistedException;
+use Oc\Repository\Exception\RecordsNotFoundException;
+
+class UserLoginBlockRepository
+{
+    private const TABLE = 'user_login_block';
+
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @throws RecordsNotFoundException
+     * @throws Exception
+     */
+    public function fetchAll(): array
+    {
+        $statement = $this->connection->createQueryBuilder()
+                ->select('*')
+                ->from(self::TABLE)
+                ->executeQuery();
+
+        $result = $statement->fetchAllAssociative();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordsNotFoundException('No records found');
+        }
+
+        $records = [];
+
+        foreach ($result as $item) {
+            $records[] = $this->getEntityFromDatabaseArray($item);
+        }
+
+        return $records;
+    }
+
+    /**
+     * @throws RecordNotFoundException
+     * @throws Exception
+     */
+    public function fetchOneBy(array $where = []): UserLoginBlockEntity
+    {
+        $queryBuilder = $this->connection->createQueryBuilder()
+                ->select('*')
+                ->from(self::TABLE)
+                ->setMaxResults(1);
+
+        if (count($where) > 0) {
+            foreach ($where as $column => $value) {
+                $queryBuilder->andWhere($column . ' = ' . $queryBuilder->createNamedParameter($value));
+            }
+        }
+
+        $statement = $queryBuilder->executeQuery();
+
+        $result = $statement->fetchAssociative();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordNotFoundException('Record with given where clause not found');
+        }
+
+        return $this->getEntityFromDatabaseArray($result);
+    }
+
+    /**
+     * @throws RecordsNotFoundException
+     * @throws Exception
+     */
+    public function fetchBy(array $where = []): array
+    {
+        $queryBuilder = $this->connection->createQueryBuilder()
+                ->select('*')
+                ->from(self::TABLE);
+
+        if (count($where) > 0) {
+            foreach ($where as $column => $value) {
+                $queryBuilder->andWhere($column . ' = ' . $queryBuilder->createNamedParameter($value));
+            }
+        }
+
+        $statement = $queryBuilder->executeQuery();
+
+        $result = $statement->fetchAllAssociative();
+
+        if ($statement->rowCount() === 0) {
+            throw new RecordsNotFoundException('No records with given where clause found');
+        }
+
+        $entities = [];
+
+        foreach ($result as $item) {
+            $entities[] = $this->getEntityFromDatabaseArray($item);
+        }
+
+        return $entities;
+    }
+
+    /**
+     * @throws RecordAlreadyExistsException
+     * @throws Exception
+     */
+    public function create(UserLoginBlockEntity $entity): UserLoginBlockEntity
+    {
+        if (!$entity->isNew()) {
+            throw new RecordAlreadyExistsException('The entity does already exist.');
+        }
+
+        $databaseArray = $this->getDatabaseArrayFromEntity($entity);
+
+        $this->connection->insert(
+                self::TABLE,
+                $databaseArray
+        );
+
+        $entity->id = (int)$this->connection->lastInsertId();
+
+        return $entity;
+    }
+
+    /**
+     * @throws RecordNotPersistedException
+     * @throws Exception
+     */
+    public function update(UserLoginBlockEntity $entity): UserLoginBlockEntity
+    {
+        if ($entity->isNew()) {
+            throw new RecordNotPersistedException('The entity does not exist.');
+        }
+
+        $databaseArray = $this->getDatabaseArrayFromEntity($entity);
+
+        $this->connection->update(
+                self::TABLE,
+                $databaseArray,
+                ['id' => $entity->id]
+        );
+
+        return $entity;
+    }
+
+    /**
+     * @throws RecordNotPersistedException
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function remove(UserLoginBlockEntity $entity): UserLoginBlockEntity
+    {
+        if ($entity->isNew()) {
+            throw new RecordNotPersistedException('The entity does not exist.');
+        }
+
+        $this->connection->delete(
+                self::TABLE,
+                ['id' => $entity->id]
+        );
+
+        $entity->id = 0;
+
+        return $entity;
+    }
+
+    public function getDatabaseArrayFromEntity(UserLoginBlockEntity $entity): array
+    {
+        return [
+                'id' => $entity->id,
+                'user_id' => $entity->userId,
+                'login_block_until' => $entity->loginBlockUntil,
+                'message' => $entity->message,
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getEntityFromDatabaseArray(array $data): UserLoginBlockEntity
+    {
+        $entity = new UserLoginBlockEntity((int)$data['user_id'], (string)$data['login_block_until'], (string)$data['message']);
+        $entity->id = (int)$data['id'];
+
+        return $entity;
+    }
+}

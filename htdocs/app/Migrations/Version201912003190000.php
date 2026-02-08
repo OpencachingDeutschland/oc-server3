a28ad9c43113321f33f6e486bb5a87ca9ba81c7d
Samuel Dennler <834472+sdennler@users.noreply.github.com>
2026-02-08 21:40:05 +0100

    Reenable gdpr_deletion columns

--- htdocs/app/Migrations/Version201912003190000.php
+++ htdocs/app/Migrations/Version201912003190000.php
@@ -11,9 +11,9 @@ final class Version201912003190000 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-//        $this->addSql('ALTER TABLE user	ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
-//        $this->addSql('ALTER TABLE caches ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
-//        $this->addSql('ALTER TABLE cache_logs ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
+        $this->addSql('ALTER TABLE user	ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
+        $this->addSql('ALTER TABLE caches ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
+        $this->addSql('ALTER TABLE cache_logs ADD gdpr_deletion TINYINT(1) DEFAULT 0 NOT NULL;');
 
         $this->addSql('UPDATE user SET gdpr_deletion = 1 WHERE username like \'delete_%\'');
     }
